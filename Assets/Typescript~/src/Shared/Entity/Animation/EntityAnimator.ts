9934ef18a2e32d5d9d4353e2d716456431803f5d
Tshadow0508 <tshadow05@gmail.com>
2023-07-10 17:11:05 -0700

    turned down footstep sound

--- Assets/Typescript~/src/Shared/Entity/Animation/EntityAnimator.ts
+++ Assets/Typescript~/src/Shared/Entity/Animation/EntityAnimator.ts
@@ -29,6 +29,7 @@ export class EntityAnimator {
 		this.anim = anim;
 		this.entityRef = entityRef;
 		this.footstepAudioBundle = new AudioClipBundle([], "Footsteps");
+		this.footstepAudioBundle.soundOptions = { volumeScale: 0.2 };
 		this.damageEffectClip = BundleReferenceManager.LoadResource<AnimationClip>(
 			BundleGroupNames.Entity,
 			Bundle_Entity.OnHit,
