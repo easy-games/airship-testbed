4397d869c05ae77966c55084df98ff352d86c3bb
Luke Chatton <chattonluke@gmail.com>
2023-09-14 11:05:30 -0700

    fix main menu

--- Assets/Bundles/Imports/Core/Core~/src/Client/MainClient.ts
+++ Assets/Bundles/Imports/Core/Core~/src/Client/MainClient.ts
@@ -1,10 +1,5 @@
 import { Flamework } from "@easy-games/flamework-core";
 import { CoreContext } from "Shared/CoreClientContext";
-import { InitNet } from "Shared/Network/NetworkAPI";
-import { TimeUtil } from "Shared/Util/TimeUtil";
-import { OnFixedUpdate, OnLateUpdate, OnTick, OnUpdate } from "Shared/Util/Timer";
-
-print("Core.MainClient");
 
 function LoadFlamework(context: CoreContext) {
 	if (context === CoreContext.GAME) {
@@ -15,22 +10,5 @@ function LoadFlamework(context: CoreContext) {
 }
 
 export function SetupClient(context: CoreContext) {
-	// Drive timer:
-	gameObject.OnUpdate(() => {
-		OnUpdate.Fire(TimeUtil.GetDeltaTime());
-	});
-	gameObject.OnLateUpdate(() => {
-		OnLateUpdate.Fire(TimeUtil.GetDeltaTime());
-	});
-	gameObject.OnFixedUpdate(() => {
-		OnFixedUpdate.Fire(TimeUtil.GetFixedDeltaTime());
-	});
-	if (InstanceFinder.TimeManager !== undefined) {
-		InstanceFinder.TimeManager.OnOnTick(() => {
-			OnTick.Fire();
-		});
-	}
-
-    InitNet();
 	LoadFlamework(context);
 }
