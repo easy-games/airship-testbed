8251434fcf7799e0c8e3824628b21a7e557bd090
Luke Chatton <chattonluke@gmail.com>
2023-09-27 16:06:00 -0700

    better handling of denying map block breaking

--- Assets/Typescript~/src/Server/Services/Match/Map/MapBlockService.ts
+++ Assets/Typescript~/src/Server/Services/Match/Map/MapBlockService.ts
@@ -1,29 +1,28 @@
-import { Dependency, OnStart, Service } from "@easy-games/flamework-core";
+import { OnStart, Service } from "@easy-games/flamework-core";
 import { ItemType } from "Core/Shared/Item/ItemType";
+import { CoreServerSignals } from "Imports/Core/Server/CoreServerSignals";
 import { BlockInteractService } from "Imports/Core/Server/Services/Block/BlockInteractService";
 import { CoreNetwork } from "Imports/Core/Shared/CoreNetwork";
 import { CharacterEntity } from "Imports/Core/Shared/Entity/Character/CharacterEntity";
 import { Entity } from "Imports/Core/Shared/Entity/Entity";
-import { items } from "Imports/Core/Shared/Item/ItemDefinitions";
 import { ItemMeta } from "Imports/Core/Shared/Item/ItemMeta";
 import { ItemUtil } from "Imports/Core/Shared/Item/ItemUtil";
 import { BlockDataAPI } from "Imports/Core/Shared/VoxelWorld/BlockData/BlockDataAPI";
 import { WorldAPI } from "Imports/Core/Shared/VoxelWorld/WorldAPI";
-import { ServerSignals } from "Server/ServerSignals";
 
 @Service({})
 export class MapBlockService implements OnStart {
 	constructor(private readonly blockService: BlockInteractService) {}
 	OnStart(): void {
-		/* Start tracking placed blocks AFTER match has started. */
-		ServerSignals.MatchStart.Connect(() => {
-			/*
-			 * Voxels placed after match started belong to players.
-			 * TODO: We _probably_ want exceptions here. IE: Lucky Blocks?
-			 */
-			WorldAPI.GetMainWorld()?.OnVoxelPlaced.Connect((pos, _voxel) => {
-				BlockDataAPI.SetBlockData(pos, "placedByUser", true);
-			});
+		CoreServerSignals.BlockPlace.Connect((event) => {
+			if (event.entity) {
+				BlockDataAPI.SetBlockData(event.pos, "canBreak", true);
+				WorldAPI.GetMainWorld()?.PlaceBlock(event.pos.add(new Vector3(0, 1, 0)), ItemType.GRASS, {
+					blockData: {
+						canBreak: true,
+					},
+				});
+			}
 		});
 
 		CoreNetwork.ClientToServer.LibonatiTest.Server.OnClientEvent((clientId) => {
