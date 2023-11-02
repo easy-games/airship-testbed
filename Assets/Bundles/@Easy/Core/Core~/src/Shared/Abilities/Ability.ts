3971da643fe01e41e0a54bd4b7c6e2aef74f1397
Jonathan Holmes <Vorlias@users.noreply.github.com>
2023-11-03 12:45:28 +1300

    Start work on abilities API

---
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/Abilities/Ability.ts
@@ -0,0 +1,3 @@
+export class Ability {
+	// TODO: API surface etc.
+}
---
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/Strollers/Abilities/AbilityRegistry.ts
@@ -0,0 +1,7 @@
+import { Controller, Service } from "@easy-games/flamework-core";
+
+@Service()
+@Controller()
+export class AbilityRegistry {
+	// TODO: API surface etc.
+}
