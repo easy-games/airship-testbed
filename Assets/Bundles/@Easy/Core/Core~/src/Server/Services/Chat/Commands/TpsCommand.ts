8f10a727a23cd66d6b6bea3ea7c0490bd08b89f7
Luke Chatton <chattonluke@gmail.com>
2023-11-24 15:13:01 -0800

    log tps on server

--- Assets/Bundles/@Easy/Core/Core~/src/Server/Services/Chat/Commands/TpsCommand.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Server/Services/Chat/Commands/TpsCommand.ts
@@ -1,10 +1,25 @@
 import { ChatCommand } from "Shared/Commands/ChatCommand";
 import { Player } from "Shared/Player/Player";
 import { ChatColor } from "Shared/Util/ChatColor";
+import { RunUtil } from "Shared/Util/RunUtil";
+import { SetInterval } from "Shared/Util/Timer";
 
 export class TpsCommand extends ChatCommand {
 	constructor() {
 		super("tps");
+
+		if (!RunUtil.IsEditor() && RunUtil.IsServer()) {
+			SetInterval(1, () => {
+				const avg = Bridge.GetAverageFPS();
+				const current = Bridge.GetCurrentFPS();
+
+				const allocatedRam = math.round(Bridge.GetAllocatedRam());
+				const reservedRam = math.round(Bridge.GetReservedRam());
+				print(
+					`TPS current: ${current}, avg: ${avg} | Ram allocated: ${allocatedRam} mb, reserved: ${reservedRam} mb`,
+				);
+			});
+		}
 	}
 
 	public Execute(player: Player, args: string[]): void {
