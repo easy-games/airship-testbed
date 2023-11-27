8021ff486c5f013baceb1520bc90cf6a6511ca13
Jonathan Holmes <Vorlias@users.noreply.github.com>
2023-11-28 10:19:42 +1300

    `EasyProjectile` -> `AirshipProjectile`, projectile changes

--- Assets/Bundles/@Easy/Core/Core~/src/Shared/Entity/Entity.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/Entity/Entity.ts
@@ -605,7 +605,7 @@ export class Entity {
 		projectileItemType: ItemType,
 		launchPos: Vector3,
 		velocity: Vector3,
-	): EasyProjectile | undefined {
+	): AirshipProjectile | undefined {
 		const itemMeta = ItemUtil.GetItemMeta(projectileItemType);
 		const launcherItemMeta = launcherItemType ? ItemUtil.GetItemMeta(launcherItemType) : undefined; // I kind of wish there was syntactic sugar for this lol
 
