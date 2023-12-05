99bb77f8146ac86efa5741c9f479ccd904703f88
liamlutton <lutton+gh@me.com>
2023-12-05 06:52:32 -0800

    Bubble chat v0

---
+++ Assets/Bundles/Types~/@Easy/Core/Client/Controllers/Chat/BubbleChat/BubbleChatController.d.ts
@@ -0,0 +1,10 @@
+import { OnStart } from "../../../../../node_modules/@easy-games/flamework-core";
+export declare class BubbleChatController implements OnStart {
+    private static MAX_DISPLAYED_MESSAGES;
+    OnStart(): void;
+    private startSendingRandomMessages;
+    private SanitizeRawChatInput;
+    private RenderBubble;
+    /** Creates a chat container for an entity (or returns one if it already exists) */
+    private GetOrCreateChatContainer;
+}
