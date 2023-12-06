917b8f10411bd1a0d675321367613c922270de44
Luke Chatton <chattonluke@gmail.com>
2023-12-06 11:34:46 -0800

    image load tracking

--- Assets/Bundles/@Easy/Core/Core~/src/Client/Components/HomePage/Sort/HomePageGameComponent.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Client/Components/HomePage/Sort/HomePageGameComponent.ts
@@ -1,21 +1,39 @@
 import { AirshipUrl } from "Shared/Util/AirshipUrl";
+import { Bin } from "Shared/Util/Bin";
 import { GameDto } from "../API/GamesAPI";
 
 export default class HomePageGameComponent extends AirshipBehaviour {
-	public titleText5!: TMP_Text;
+	public titleText!: TMP_Text;
+	public playerCountText!: TMP_Text;
 	public remoteImage!: RemoteImage;
+	private bin = new Bin();
 
 	override OnStart(): void {}
 
-	override OnDestroy(): void {}
+	override OnDestroy(): void {
+		this.bin.Clean();
+	}
 
 	public Init(gameDto: GameDto) {
 		// this.titleText.GetComponent<TMP_Text>().text = gameDto;
-		this.titleText5.text = "Hi";
+		this.titleText.text = gameDto.name;
+		this.playerCountText.text = gameDto.liveStats.playerCount + "";
 
 		let url = AirshipUrl.CDN + gameDto.icon;
 
-		this.remoteImage.url = url;
-		this.remoteImage.StartDownload();
+		let remoteImage = this.gameObject.transform.GetChild(0).GetComponent<RemoteImage>();
+		remoteImage.url = url;
+		remoteImage.StartDownload();
+
+		const downloadConn = remoteImage.OnFinishedLoading((success) => {
+			if (success) {
+				remoteImage.image.color = new Color(1, 1, 1, 1);
+			} else {
+				remoteImage.image.color = new Color(0, 0, 0, 0.3);
+			}
+		});
+		this.bin.Add(() => {
+			Bridge.DisconnectEvent(downloadConn);
+		});
 	}
 }
