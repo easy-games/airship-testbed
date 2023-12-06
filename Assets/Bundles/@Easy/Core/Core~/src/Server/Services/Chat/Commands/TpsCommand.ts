34f700f112dd1c6ecc68d6cc5a253781a4676f6a
Luke Chatton <chattonluke@gmail.com>
2023-12-06 11:46:46 -0800

    fix main script

--- Assets/Bundles/@Easy/Core/Core~/src/Server/Services/Chat/Commands/TpsCommand.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Server/Services/Chat/Commands/TpsCommand.ts
@@ -1,26 +1,24 @@
 import { ChatCommand } from "Shared/Commands/ChatCommand";
 import { Player } from "Shared/Player/Player";
 import { ChatColor } from "Shared/Util/ChatColor";
-import { RunUtil } from "Shared/Util/RunUtil";
-import { SetInterval } from "Shared/Util/Timer";
 
 export class TpsCommand extends ChatCommand {
 	constructor() {
 		super("tps");
 
-		if (!RunUtil.IsEditor() && RunUtil.IsServer()) {
-			SetInterval(1, () => {
-				const avg = Bridge.GetAverageFPS();
-				const current = Bridge.GetCurrentFPS();
+		// if (!RunUtil.IsEditor() && RunUtil.IsServer()) {
+		// 	SetInterval(1, () => {
+		// 		const avg = Bridge.GetAverageFPS();
+		// 		const current = Bridge.GetCurrentFPS();
 
-				const monoRam = math.round(Bridge.GetMonoRam());
-				const allocatedRam = math.round(Bridge.GetAllocatedRam());
-				const reservedRam = math.round(Bridge.GetReservedRam());
-				print(
-					`TPS current: ${current}, avg: ${avg} | Ram mono: ${monoRam} mb, allocated: ${allocatedRam} mb, reserved: ${reservedRam} mb`,
-				);
-			});
-		}
+		// 		const monoRam = math.round(Bridge.GetMonoRam());
+		// 		const allocatedRam = math.round(Bridge.GetAllocatedRam());
+		// 		const reservedRam = math.round(Bridge.GetReservedRam());
+		// 		print(
+		// 			`TPS current: ${current}, avg: ${avg} | Ram mono: ${monoRam} mb, allocated: ${allocatedRam} mb, reserved: ${reservedRam} mb`,
+		// 		);
+		// 	});
+		// }
 	}
 
 	public Execute(player: Player, args: string[]): void {
