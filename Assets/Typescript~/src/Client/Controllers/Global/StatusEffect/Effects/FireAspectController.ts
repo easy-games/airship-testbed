226b2710d65d2ae049327d73f32a2c9ddbee63fa
Rob-bie <deibbor@gmail.com>
2023-12-13 19:13:10 -0600

    Support immunity by damage type.

--- Assets/Typescript~/src/Client/Controllers/Global/StatusEffect/Effects/FireAspectController.ts
+++ Assets/Typescript~/src/Client/Controllers/Global/StatusEffect/Effects/FireAspectController.ts
@@ -1,5 +1,6 @@
 import { CoreClientSignals } from "@Easy/Core/Client/CoreClientSignals";
 import { DamageType } from "@Easy/Core/Shared/Damage/DamageType";
+import { EffectsManager } from "@Easy/Core/Shared/Effects/EffectsManager";
 import { CharacterEntity } from "@Easy/Core/Shared/Entity/Character/CharacterEntity";
 import { Controller, OnStart } from "@easy-games/flamework-core";
 import { StatusEffectType } from "Shared/StatusEffect/StatusEffectType";
@@ -21,8 +22,16 @@ export class FireAspectController implements OnStart {
 				)
 			) {
 				// TODO: Play some cool effects here when an entity is hit by an entity
-				// that has fire aspect. We should also probably on the entity while they're
-				// on fire.
+				// that has fire aspect. We should also probably play an effect on the entity
+				// while they're on fire.
+				const effectGO = EffectsManager.SpawnPrefabEffect(
+					"@Easy/Core/Shared/Resources/VFX/Yos/Prefab/Explo_Hit.prefab",
+					event.entity.GetMiddlePosition().add(new Vector3(0, 0.5, 0)),
+					new Vector3(),
+					0.25,
+				);
+				effectGO.transform.localScale = new Vector3(0.25, 0.25, 0.25);
+				effectGO.transform.SetParent(event.entity.model.transform);
 			}
 		});
 	}
