3007127900e124d610dd08f7f712aa5488e1c58d
Jonathan Holmes <Vorlias@users.noreply.github.com>
2023-12-16 09:33:51 +1300

    Added team chat command

--- Assets/Bundles/Types~/@Easy/Core/Server/Services/Chat/ChatService.d.ts
+++ Assets/Bundles/Types~/@Easy/Core/Server/Services/Chat/ChatService.d.ts
@@ -1,11 +1,22 @@
 import { OnStart } from "../../../../node_modules/@easy-games/flamework-core";
 import { ChatCommand } from "../../../Shared/Commands/ChatCommand";
 import { PlayerService } from "../Player/PlayerService";
+import { Player } from "../../../Shared/Player/Player";
 export declare class ChatService implements OnStart {
     private readonly playerService;
     private commands;
+    readonly CanUseRichText = true;
     constructor(playerService: PlayerService);
     RegisterCommand(command: ChatCommand): void;
+    /**
+     *
+     * @internal
+     * @param player The player
+     * @param message The chat message
+     * @param canRichText Whether or not the chat message allows rich text
+     * @returns
+     */
+    FormatUserChatMessage(player: Player, message: string, canRichText?: boolean): string;
     OnStart(): void;
     GetCommands(): ChatCommand[];
 }
