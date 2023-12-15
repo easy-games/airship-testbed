20f8f5cf1f9c7e4551c31ab71e7f18eba62b4b28
Jonathan Holmes <Vorlias@users.noreply.github.com>
2023-12-15 13:42:41 +1300

    Use levenshtein distance sorting to likely name

--- Assets/Bundles/@Easy/Core/Core~/src/Shared/Types/Levenshtein.d.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/Types/Levenshtein.d.ts
@@ -1,2 +1,10 @@
-declare function LevenshteinDistance(string1: string, string2: string): number;
-export { LevenshteinDistance };
+/**
+ * Calculates the Levenshtein distance of two strings. This function is better than most because it takes utf8 characters into account.
+ *
+ * More information on it can be seen [here, on wikipedia](https://en.wikipedia.org/wiki/Levenshtein_distance).
+ * @param str The string
+ * @param cmp The string to compare to
+ * @returns A "distance" number - the higher, the further `str` is from `cmp`
+ */
+declare function Levenshtein(str: string, cmp: string): number;
+export { Levenshtein };
