7f43518e9a560bc11eb9b8acf4b9f2d569de6042
Luke Chatton <chattonluke@gmail.com>
2023-12-29 08:58:54 -0800

    spawn with some items

--- Assets/Bundles/@Easy/Core/Core~/src/Shared/MapEdit/MapEditComponent.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/MapEdit/MapEditComponent.ts
@@ -3,6 +3,8 @@ import { LoadingScreenController } from "Client/Controllers/Loading/LoadingScree
 import { EntityService } from "Server/Services/Entity/EntityService";
 import { PlayerService } from "Server/Services/Player/PlayerService";
 import { EntityPrefabType } from "Shared/Entity/EntityPrefabType";
+import { ItemStack } from "Shared/Inventory/ItemStack";
+import { ItemType } from "Shared/Item/ItemType";
 import { RunUtil } from "Shared/Util/RunUtil";
 import { WorldAPI } from "Shared/VoxelWorld/WorldAPI";
 
@@ -14,12 +16,16 @@ export default class MapEditComponent extends AirshipBehaviour {
 		if (RunUtil.IsServer()) {
 			world.LoadWorldFromSaveFile(world.voxelWorld.voxelWorldFile);
 			Dependency<PlayerService>().ObservePlayers((p) => {
-				Dependency<EntityService>().SpawnPlayerEntity(
+				const entity = Dependency<EntityService>().SpawnPlayerEntity(
 					p,
 					EntityPrefabType.HUMAN,
 					this.spawnPosition.position,
 					this.spawnPosition.rotation,
 				);
+				const inv = entity.GetInventory();
+				inv.AddItem(new ItemStack(ItemType.DIAMOND_PICKAXE, 1));
+				inv.AddItem(new ItemStack(ItemType.GRASS, 9999));
+				inv.AddItem(new ItemStack(ItemType.STONE, 9999));
 			});
 		}
 
