76983cfce127066c92526f1a62457199011aa1f0
Rob-bie <deibbor@gmail.com>
2024-01-04 11:55:59 -0600

    Enchant table component.

---
+++ Assets/Typescript~/src/Shared/Behaviours/EnchantTableComponent.ts
@@ -0,0 +1,7 @@
+export default class EnchantTableComponent extends AirshipBehaviour {
+	public override Awake(): void {}
+
+	public override Start(): void {}
+
+	public override OnDestroy(): void {}
+}
