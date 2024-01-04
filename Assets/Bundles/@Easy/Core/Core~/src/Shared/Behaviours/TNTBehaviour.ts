c7aa9e4ceb799f5ee609188f61ef597d0edbd130
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-01-04 22:15:43 +1300

    Client TNT

--- Assets/Bundles/@Easy/Core/Core~/src/Shared/Behaviours/TNTBehaviour.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/Behaviours/TNTBehaviour.ts
@@ -1,7 +1,10 @@
 import { Dependency } from "@easy-games/flamework-core";
 import { BlockInteractService } from "Server/Services/Block/BlockInteractService";
+import { EffectsManager } from "Shared/Effects/EffectsManager";
+import { CharacterEntity } from "Shared/Entity/Character/CharacterEntity";
 import { GameObjectUtil } from "Shared/GameObject/GameObjectUtil";
 import { ItemType } from "Shared/Item/ItemType";
+import { AllBundleItems } from "Shared/Util/ReferenceManagerResources";
 import { RunUtil } from "Shared/Util/RunUtil";
 import { WorldAPI } from "Shared/VoxelWorld/WorldAPI";
 
@@ -13,7 +16,7 @@ export default class TntBehaviour extends AirshipBehaviour {
 	public rigidBody!: Rigidbody;
 
 	public Awake(): void {
-		if (RunUtil.IsClient() || this.renderModelOnServer) {
+		if (RunUtil.IsClient()) {
 			const world = WorldAPI.GetMainWorld();
 			if (world) {
 				const tntId = world.voxelWorld.blocks.GetBlockIdFromStringId(ItemType.TNT);
@@ -25,7 +28,7 @@ export default class TntBehaviour extends AirshipBehaviour {
 		}
 
 		// TSSSSSST... 💥
-		this.rigidBody = this.gameObject.AddComponent<Rigidbody>();
+		this.rigidBody = this.gameObject.GetComponent<Rigidbody>();
 		this.LightFuse();
 	}
 
@@ -39,12 +42,19 @@ export default class TntBehaviour extends AirshipBehaviour {
 	public Detonate() {
 		if (RunUtil.IsServer()) {
 			Dependency(BlockInteractService).DamageBlockAOE(undefined, this.gameObject.transform.position, {
-				damageRadius: 30,
-				blockExplosiveDamage: 30,
+				damageRadius: 10,
+				blockExplosiveDamage: 10,
 				innerDamage: 30,
-				outerDamage: 30,
+				outerDamage: 10,
 			});
+		} else {
+			EffectsManager.SpawnPrefabEffect(
+				AllBundleItems.Projectiles_OnHitVFX_FireballExplosion,
+				this.gameObject.transform.position,
+				Vector3.zero,
+			);
 		}
+
 		GameObjectUtil.Destroy(this.gameObject);
 	}
 }
