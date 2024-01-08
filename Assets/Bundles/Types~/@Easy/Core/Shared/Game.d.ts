e2320c8b1629aced3c753b41a10d1d2e9ee64527
Luke Chatton <chattonluke@gmail.com>
2024-01-07 19:27:59 -0800

    types

--- Assets/Bundles/Types~/@Easy/Core/Shared/Game.d.ts
+++ Assets/Bundles/Types~/@Easy/Core/Shared/Game.d.ts
@@ -1,7 +1,11 @@
 import { CoreContext } from "./CoreClientContext";
 import { Player } from "./Player/Player";
+import { Signal } from "./Util/Signal";
 export declare class Game {
     static localPlayer: Player;
+    static localPlayerLoaded: boolean;
+    static onLocalPlayerLoaded: Signal<void>;
+    static WaitForLocalPlayerLoaded(): void;
     static BroadcastMessage(message: string): void;
     static context: CoreContext;
     static serverId: string;
