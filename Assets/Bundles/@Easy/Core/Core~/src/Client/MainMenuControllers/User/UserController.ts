513120cab142613101e98548fd7673eea1381f1a
Luke Chatton <chattonluke@gmail.com>
2024-01-07 19:20:17 -0800

    add api for knowing when local player is loaded

--- Assets/Bundles/@Easy/Core/Core~/src/Client/MainMenuControllers/User/UserController.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Client/MainMenuControllers/User/UserController.ts
@@ -41,6 +41,8 @@ export class UserController implements OnStart {
 			writeUser.userId = data.uid;
 			writeUser.username = data.username;
 			writeUser.usernameTag = data.discriminator;
+			Game.localPlayerLoaded = true;
+            Game.onLocalPlayerLoaded.Fire();
 
 			this.onLocalUserUpdated.Fire(this.localUser);
 			return;
--- Assets/Bundles/@Easy/Core/Core~/src/Shared/Game.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/Game.ts
@@ -2,6 +2,7 @@ import { CoreContext } from "./CoreClientContext";
 import { CoreNetwork } from "./CoreNetwork";
 import { Player } from "./Player/Player";
 import { RunUtil } from "./Util/RunUtil";
+import { Signal } from "./Util/Signal";
 
 export class Game {
 	public static localPlayer: Player = new Player(
@@ -11,6 +12,13 @@ export class Game {
 		"LocalPlayer",
 		"null",
 	);
+	public static localPlayerLoaded = false;
+	public static onLocalPlayerLoaded = new Signal<void>();
+
+	public static WaitForLocalPlayerLoaded(): void {
+		if (this.localPlayerLoaded) return;
+		this.onLocalPlayerLoaded.Wait();
+	}
 
 	public static BroadcastMessage(message: string): void {
 		if (RunUtil.IsServer()) {
@@ -20,7 +28,7 @@ export class Game {
 		}
 	}
 
-	public static context: CoreContext;
+	public static coreContext: CoreContext;
 
 	public static serverId: string;
 	public static gameId: string;
