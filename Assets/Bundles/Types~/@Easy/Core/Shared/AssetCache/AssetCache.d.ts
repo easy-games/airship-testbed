82559cc01383525b5a6179c54812c925807ef91f
Luke Chatton <chattonluke@gmail.com>
2024-01-09 15:39:17 -0800

    improve typing

--- Assets/Bundles/Types~/@Easy/Core/Shared/AssetCache/AssetCache.d.ts
+++ Assets/Bundles/Types~/@Easy/Core/Shared/AssetCache/AssetCache.d.ts
@@ -1,5 +1,21 @@
 export declare class AssetCache {
     private static loadedAssets;
+    /**
+     * Loads a file from game bundle.
+     *
+     * Example path: "Shared/Resources/Test.prefab"
+     *
+     * Make sure to include a file extension (example: `.prefab` or `.png`)
+     * @returns
+     */
     static LoadAsset<T = GameObject>(path: string): T;
+    /**
+     * Loads a file from game bundle.
+     *
+     * Example path: "Shared/Resources/Test.prefab"
+     *
+     * Make sure to include a file extension (example: `.prefab` or `.png`)
+     * @returns
+     */
     static LoadAssetIfExists<T = GameObject>(path: string): T | undefined;
 }
