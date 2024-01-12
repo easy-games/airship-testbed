334398570d02fe5c2f6df3f8bfb19ae48d9abe68
Jack Schaffner <jackwschaffner@gmail.com>
2024-01-12 13:10:37 -0800

    Fix region priority, add event controller

--- Assets/Bundles/@Easy/Core/Core~/src/Client/MainMenuControllers/Socket/SocketController.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Client/MainMenuControllers/Socket/SocketController.ts
@@ -33,7 +33,7 @@ export class SocketController implements OnStart {
 					InternalHttpManager.PutAsync(
 						AirshipUrl.GameCoordinator + "/user-session/data",
 						EncodeJSON({
-							selectedRegion: "na",
+							regionPriority: ["na"],
 						}),
 					);
 				},
