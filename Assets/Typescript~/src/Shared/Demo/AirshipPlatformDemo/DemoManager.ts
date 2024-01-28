2288c85128e5065c79bd0dc4a6f7f10e03facf91
Luke Chatton <chattonluke@gmail.com>
2024-01-28 11:12:37 -0800

    update platform demo UI

--- Assets/Typescript~/src/Shared/Demo/AirshipPlatformDemo/DemoManager.ts
+++ Assets/Typescript~/src/Shared/Demo/AirshipPlatformDemo/DemoManager.ts
@@ -1,5 +1,7 @@
 import { Airship } from "@Easy/Core/Shared/Airship";
 import Character from "@Easy/Core/Shared/Character/Character";
+import { ItemStack } from "@Easy/Core/Shared/Inventory/ItemStack";
+import { ItemType } from "@Easy/Core/Shared/Item/ItemType";
 import { Player } from "@Easy/Core/Shared/Player/Player";
 import { Bin } from "@Easy/Core/Shared/Util/Bin";
 import { RunUtil } from "@Easy/Core/Shared/Util/RunUtil";
@@ -32,8 +34,7 @@ export default class DemoManager extends AirshipBehaviour {
 
 	public SpawnPlayer(player: Player): void {
 		const character = player.SpawnCharacter(this.spawnPosition.transform.position);
-		// const inv = entity.GetInventory();
-		// inv.AddItem(new ItemStack(ItemType.STONE_SWORD));
+		character.inventory.AddItem(new ItemStack(ItemType.WOOD_SWORD));
 	}
 
 	override OnDestroy(): void {
