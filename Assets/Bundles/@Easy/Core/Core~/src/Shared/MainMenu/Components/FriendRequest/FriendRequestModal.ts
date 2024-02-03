3b73c06eeb05332c95b0e651d2b2324a042d4cfe
Luke Chatton <chattonluke@gmail.com>
2024-02-02 20:26:44 -0800

    more friends impl

---
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/MainMenu/Components/FriendRequest/FriendRequestModal.ts
@@ -0,0 +1,34 @@
+import { Dependency } from "@easy-games/flamework-core";
+import { FriendsController } from "Client/MainMenuControllers/Social/FriendsController";
+import { Bin } from "Shared/Util/Bin";
+import IncomingFriendRequestCard from "./IncomingFriendRequestCard";
+
+export default class FriendRequestModal extends AirshipBehaviour {
+	public content!: Transform;
+	public friendRequestPrefab!: GameObject;
+	private bin = new Bin();
+
+	override Start(): void {
+		this.AddCards();
+		this.bin.Add(
+			Dependency<FriendsController>().onIncomingFriendRequestsChanged.Connect(() => {
+				this.AddCards();
+			}),
+		);
+	}
+
+	private AddCards(): void {
+		this.content.gameObject.ClearChildren();
+
+		const friendsController = Dependency<FriendsController>();
+		for (const user of friendsController.incomingFriendRequests) {
+			const go = Object.Instantiate(this.friendRequestPrefab, this.content);
+			const card = go.GetAirshipComponent<IncomingFriendRequestCard>()!;
+			card.Init(user);
+		}
+	}
+
+	override OnDestroy(): void {
+		this.bin.Clean();
+	}
+}
---
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/MainMenu/Components/SocialFriendRequestsButtonComponent.ts
@@ -0,0 +1,30 @@
+import { AppManager } from "Shared/Util/AppManager";
+import { CanvasAPI } from "Shared/Util/CanvasAPI";
+
+export default class SocialFriendRequestsButtonComponent extends AirshipBehaviour {
+	public text!: TMP_Text;
+	public button!: Button;
+
+	override Start(): void {
+		CanvasAPI.OnClickEvent(this.button.gameObject, () => {
+			const go = Object.Instantiate(
+				AssetBridge.Instance.LoadAsset(
+					"@Easy/Core/Shared/Resources/Prefabs/UI/MainMenu/FriendRequests/FriendRequestsModal.prefab",
+				),
+			);
+			const canvas = go.GetComponent<Canvas>();
+			canvas.enabled = false;
+
+			AppManager.Open(canvas, {
+				noOpenSound: true,
+				addToStack: true,
+				sortingOrderOffset: 100,
+				onClose: () => {
+					Object.Destroy(canvas.gameObject);
+				},
+			});
+		});
+	}
+
+	override OnDestroy(): void {}
+}
