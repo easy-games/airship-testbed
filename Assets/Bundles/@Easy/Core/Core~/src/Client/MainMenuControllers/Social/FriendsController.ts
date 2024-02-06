7d7b11673532c1cd46d1a03458fd027f7974684c
Luke Chatton <chattonluke@gmail.com>
2024-02-06 09:40:02 -0800

    main menu fixes

--- Assets/Bundles/@Easy/Core/Core~/src/Client/MainMenuControllers/Social/FriendsController.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Client/MainMenuControllers/Social/FriendsController.ts
@@ -125,7 +125,7 @@ export class FriendsController implements OnStart {
 								break;
 							}
 						}
-						if (found) {
+						if (!found) {
 							this.socialNotification.gameObject.SetActive(false);
 						}
 					}),
