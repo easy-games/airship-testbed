5aa0dc0d4c3250e7c505a74b51459caf8ba2a4bb
Luke Chatton <chattonluke@gmail.com>
2024-02-14 14:01:43 -0800

    update main menu

--- Assets/Bundles/@Easy/Core/Core~/src/Shared/MainMenu/Components/MainMenuNavButton.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/MainMenu/Components/MainMenuNavButton.ts
@@ -1,10 +1,11 @@
 import { CanvasAPI, HoverState } from "Shared/Util/CanvasAPI";
-import { ColorUtil } from "Shared/Util/ColorUtil";
 
 export default class MainMenuNavButton extends AirshipBehaviour {
 	public selected = false;
 	public image!: Image;
 	public trueShadow!: TrueShadow;
+	// public selectedColor!: Color;
+	// public unselectedColor!: Color;
 
 	public Awake(): void {
 		this.image = this.gameObject.GetComponent<Image>();
@@ -13,12 +14,11 @@ export default class MainMenuNavButton extends AirshipBehaviour {
 
 	override Start(): void {
 		CanvasAPI.OnHoverEvent(this.gameObject, (state) => {
-			if (this.selected) return;
-
+			// if (this.selected) return;
 			if (state === HoverState.ENTER) {
 				this.image.TweenGraphicAlpha(0.6, 0.12);
 			} else {
-				this.image.TweenGraphicAlpha(0.9, 0.12);
+				this.image.TweenGraphicAlpha(this.selected ? 0.7 : 0.9, 0.12);
 			}
 		});
 	}
@@ -28,7 +28,8 @@ export default class MainMenuNavButton extends AirshipBehaviour {
 	public SetSelected(val: boolean): void {
 		this.selected = val;
 
-		this.image.color = val ? new Color(1, 1, 1, 0.07) : ColorUtil.HexToColor("18191A");
+		this.image.color = val ? new Color(1, 1, 1, 0.1) : new Color(0.09, 0.08, 0.08, 0.94);
+		// this.image.color = val ? this.selectedColor : this.unselectedColor;
 		// this.image.TweenGraphicColor(val ? new Color(1, 1, 1, 0.05) : ColorUtil.HexToColor("18191A"), 0.12);
 		this.trueShadow.enabled = !val;
 	}
