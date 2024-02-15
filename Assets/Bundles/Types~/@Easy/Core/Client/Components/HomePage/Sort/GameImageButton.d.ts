e0a2e1d234b1f3ffeb413416d9a7484b73f3a34b
Luke Chatton <chattonluke@gmail.com>
2024-02-14 18:11:45 -0800

    build

---
+++ Assets/Bundles/Types~/@Easy/Core/Client/Components/HomePage/Sort/GameImageButton.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="@easy-games/compiler-types" />
+export default class GameImageButton extends AirshipBehaviour {
+    private bin;
+    Start(): void;
+    OnDestroy(): void;
+}
---
+++ Assets/Bundles/Types~/@Easy/Core/Shared/MainMenu/Components/ProfileOptionsButton.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="@easy-games/compiler-types" />
+export default class ProfileOptionsButton extends AirshipBehaviour {
+    Start(): void;
+    OnDestroy(): void;
+}
