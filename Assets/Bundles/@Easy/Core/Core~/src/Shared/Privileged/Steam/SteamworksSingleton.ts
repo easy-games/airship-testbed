82429919c451de1278b6163dd191a082794a6986
liamlutton <lutton+gh@me.com>
2024-02-16 15:59:39 -0800

    Cleanup logs

--- Assets/Bundles/@Easy/Core/Core~/src/Shared/Privileged/Steam/SteamworksSingleton.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/Privileged/Steam/SteamworksSingleton.ts
@@ -15,7 +15,6 @@ interface SteamConnectPacket {
 export class SteamworksSingleton implements OnStart {
 	OnStart(): void {
 		SteamLuauAPI.OnRichPresenceGameJoinRequest((connectionStr, steamId) => {
-			print("Received steam join request. ConnectionStr: " + connectionStr);
 			const connectInfo = DecodeJSON<SteamConnectPacket>(connectionStr);
 			Dependency<TransferController>().TransferToGameAsync(connectInfo.gameId, connectInfo.serverId);
 		});
