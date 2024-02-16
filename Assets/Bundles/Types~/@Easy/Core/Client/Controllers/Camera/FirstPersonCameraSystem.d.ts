0e233a72b941ec8738c7ebe2e2bb692a578dbab2
Luke Chatton <chattonluke@gmail.com>
2024-02-15 16:52:41 -0800

    build

--- Assets/Bundles/Types~/@Easy/Core/Client/Controllers/Camera/FirstPersonCameraSystem.d.ts
+++ Assets/Bundles/Types~/@Easy/Core/Client/Controllers/Camera/FirstPersonCameraSystem.d.ts
@@ -11,7 +11,7 @@ export declare class FirstPersonCameraSystem {
     private currentBobData;
     private targetBobStrength;
     private currentBobStrength;
-    private manualSpineOffset;
+    private headSpineOffset;
     private inFirstPerson;
     private bin;
     private currentTime;
