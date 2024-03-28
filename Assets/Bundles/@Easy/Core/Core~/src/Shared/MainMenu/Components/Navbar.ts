bb6812bf429dcb8c9ee18c10b458a4570c78725f
Luke Chatton <chattonluke@gmail.com>
2024-03-28 16:13:24 -0700

    mobile improvements

---
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/MainMenu/Components/Navbar.ts
@@ -0,0 +1,50 @@
+import { CoreContext } from "../../CoreClientContext";
+import { Dependency } from "../../Flamework";
+import { Game } from "../../Game";
+import { Bin } from "../../Util/Bin";
+import { MainMenuSingleton } from "../Singletons/MainMenuSingleton";
+
+export default class Navbar extends AirshipBehaviour {
+	@Header("References")
+	public leftContent!: RectTransform;
+	public rightContent!: RectTransform;
+	public rightLayoutGroup!: HorizontalLayoutGroup;
+	public runningGameBtn!: RectTransform;
+	public myGamesBtn!: RectTransform;
+	public homeBtn!: RectTransform;
+
+	private bin = new Bin();
+
+	override OnEnable(): void {
+		const rect = this.transform as RectTransform;
+		const mainMenu = Dependency<MainMenuSingleton>();
+		this.bin.Add(
+			mainMenu.ObserveScreenSize((st, size) => {
+				// if (st === "sm") {
+				// 	this.runningGameBtn.SetParent(this.leftContent);
+				// } else {
+				// 	this.runningGameBtn.SetParent(this.rightContent);
+				// 	this.runningGameBtn.SetSiblingIndex(0);
+				// }
+
+				if (Game.IsLandscape() && st === "sm") {
+					rect.offsetMin = new Vector2(46, rect.offsetMin.y);
+					this.rightLayoutGroup.padding.right = 55;
+					Bridge.UpdateLayout(this.rightLayoutGroup.transform, false);
+				} else {
+					rect.offsetMin = new Vector2(0, rect.offsetMin.y);
+					this.rightLayoutGroup.padding.right = 0;
+				}
+
+				if (Game.coreContext === CoreContext.GAME && st === "sm") {
+					this.homeBtn.gameObject.SetActive(false);
+					this.myGamesBtn.gameObject.SetActive(false);
+				}
+			}),
+		);
+	}
+
+	override OnDisable(): void {
+		this.bin.Clean();
+	}
+}
