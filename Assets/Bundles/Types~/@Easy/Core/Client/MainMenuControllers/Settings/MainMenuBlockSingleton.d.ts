31bf9d59465adce3d16fec267770f4df665c27c3
Luke Chatton <chattonluke@gmail.com>
2024-04-14 13:52:04 -0700

    fix assignments

--- Assets/Bundles/Types~/@Easy/Core/Client/MainMenuControllers/Settings/MainMenuBlockSingleton.d.ts
+++ Assets/Bundles/Types~/@Easy/Core/Client/MainMenuControllers/Settings/MainMenuBlockSingleton.d.ts
@@ -4,13 +4,22 @@ interface BlockedGame {
     id: string;
     name: string;
 }
+interface BlockedUser {
+    uid: string;
+    username: string;
+}
 export declare class MainMenuBlockSingleton implements OnStart {
     blockedGameIds: Set<string>;
     blockedGames: BlockedGame[];
+    blockedUserIds: Set<string>;
+    blockedUsers: BlockedUser[];
     OnStart(): void;
     BlockGameAsync(gameId: string, gameName: string): void;
     UnblockGameAsync(gameId: string): void;
     IsGameIdBlocked(gameId: string): boolean;
+    BlockUserAsync(uid: string, username: string): void;
+    UnblockUserAsync(uid: string): void;
+    IsUserIdBlocked(uid: string): boolean;
     private SaveToDisk;
 }
 export {};
