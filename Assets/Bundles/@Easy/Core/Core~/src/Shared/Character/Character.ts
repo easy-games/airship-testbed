14765cb199f617012da1cf946a837e091b2ddb21
Luke Chatton <chattonluke@gmail.com>
2024-05-03 13:49:42 -0700

    fix teleport

--- Assets/Bundles/@Easy/Core/Core~/src/Shared/Character/Character.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/Character/Character.ts
@@ -147,7 +147,11 @@ export default class Character extends AirshipBehaviour {
 	 * You can call it from the client only when using Client Authoratative characters.
 	 */
 	public Teleport(pos: Vector3, lookDirection?: Vector3): void {
-		this.movement.Teleport(pos);
+		if (lookDirection) {
+			this.movement.Teleport(pos, Quaternion.LookRotation(lookDirection));
+		} else {
+			this.movement.Teleport(pos, Quaternion.identity);
+		}
 	}
 
 	/**
---
+++ Assets/Bundles/Types~/@Easy/Core/Shared/MainMenu/Components/Settings/DevConsoleButton.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="compiler-types" />
+export default class DevConsoleButton extends AirshipBehaviour {
+    private bin;
+    OnEnable(): void;
+    OnDisable(): void;
+}
