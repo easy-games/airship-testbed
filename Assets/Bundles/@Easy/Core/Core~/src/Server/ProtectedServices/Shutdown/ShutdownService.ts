02f171d8deba36c5b759403a5e348cd681051de6
Luke Chatton <chattonluke@gmail.com>
2024-05-15 15:12:43 -0700

    fix shutdown service

--- Assets/Bundles/@Easy/Core/Core~/src/Server/ProtectedServices/Shutdown/ShutdownService.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Server/ProtectedServices/Shutdown/ShutdownService.ts
@@ -1,6 +1,5 @@
-import { Airship } from "@Easy/Core/Shared/Airship";
 import { OnStart, Service } from "@Easy/Core/Shared/Flamework";
-import { RunUtil } from "@Easy/Core/Shared/Util/RunUtil";
+import { Game } from "@Easy/Core/Shared/Game";
 import { SetInterval } from "@Easy/Core/Shared/Util/Timer";
 
 @Service({})
@@ -14,23 +13,32 @@ export class ShutdownService implements OnStart {
 	constructor() {}
 
 	OnStart(): void {
-		Airship.players.onPlayerJoined.Connect((player) => {
-			this.playerConnected = true;
-			this.timeWithNoPlayers = 0;
-		});
+		// Airship.players.onPlayerJoined.Connect((player) => {
+		// 	this.playerConnected = true;
+		// 	this.timeWithNoPlayers = 0;
+		// });
 
 		const intervalTime = 10;
 		SetInterval(intervalTime, () => {
-			let realPlayerCount = Airship.players
-				.GetPlayers()
-				.filter((p) => !p.IsBot())
-				.size();
-			if (realPlayerCount === 0) {
-				this.timeWithNoPlayers += intervalTime;
+			if (Game.IsEditor()) {
+				return;
+			}
 
-				if (RunUtil.IsEditor()) {
-					return;
+			let playerCount = 0;
+			const players = PlayerManagerBridge.Instance.GetPlayers();
+			// filter bots
+			for (let i = 0; i < players.Length; i++) {
+				const p = players.GetValue(i);
+				if (p.clientId >= 0) {
+					playerCount++;
 				}
+			}
+			if (playerCount > 0) {
+				this.playerConnected = true;
+				this.timeWithNoPlayers = 0;
+			}
+			if (playerCount === 0) {
+				this.timeWithNoPlayers += intervalTime;
 
 				if (this.playerConnected) {
 					if (this.timeWithNoPlayers >= ShutdownService.shutdownTimeAllPlayersLeft) {
