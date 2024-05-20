2bcc54fe0b1e5fd9d9183e7594c9a528c92129e9
Rob-bie <deibbor@gmail.com>
2024-05-20 17:37:58 -0500

    Fix `FromArray`.

--- Assets/Bundles/@Easy/Core/Core~/src/Shared/Util/RandomUtil.ts
+++ Assets/Bundles/@Easy/Core/Core~/src/Shared/Util/RandomUtil.ts
@@ -5,8 +5,10 @@ export class RandomUtil {
 	 * @returns A random element from `array`.
 	 */
 	public static FromArray<T>(array: T[]): T {
-		let index = 0;
-		if (array.size() > 1) math.random(0, array.size() - 1);
+		if (array.size() <= 0) {
+			error("Unable to fetch random element from empty array.");
+		}
+		let index = math.random(0, array.size() - 1);
 		return array[index];
 	}
 }
