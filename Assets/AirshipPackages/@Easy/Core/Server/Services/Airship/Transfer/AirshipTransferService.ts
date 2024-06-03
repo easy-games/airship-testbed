518462dc60e26e09272bdbdefa64c535ff2f8795
Jack Schaffner <jackwschaffner@gmail.com>
2024-06-03 12:07:37 -0700

    Add updated transfer APIs

--- Assets/AirshipPackages/@Easy/Core/Server/Services/Airship/Transfer/AirshipTransferService.ts
+++ Assets/AirshipPackages/@Easy/Core/Server/Services/Airship/Transfer/AirshipTransferService.ts
@@ -5,6 +5,7 @@ import {
 	TransferServiceBridgeTopics,
 } from "@Easy/Core/Server/ProtectedServices/Airship/Transfer/TransferService";
 import { Platform } from "@Easy/Core/Shared/Airship";
+import { AirshipServerConfig } from "@Easy/Core/Shared/Airship/Types/Inputs/AirshipTransfers";
 import { AirshipUtil } from "@Easy/Core/Shared/Airship/Util/AirshipUtil";
 import { OnStart, Service } from "@Easy/Core/Shared/Flamework";
 import { Game } from "@Easy/Core/Shared/Game";
@@ -27,14 +28,14 @@ export class TransferService implements OnStart {
 
 	/**
 	 * Creates a new server and returns a server id which can be used to transfer players to the new server.
-	 * @param sceneId The sceneId the server should start with. If not provided, the server will use the default
-	 * scene provided during deployment.
+	 * @param config The configuration the server should start with. If not provided, the server will use the defaults
+	 * provided during deployment.
 	 * @returns The id of the new server. Undefined if the server was not able to be created.
 	 */
-	public async CreateServer(sceneId?: string): Promise<Result<CreateServerResponse, undefined>> {
+	public async CreateServer(config?: AirshipServerConfig): Promise<Result<CreateServerResponse, undefined>> {
 		return await AirshipUtil.PromisifyBridgeInvoke<ServerBridgeApiCreateServer>(
 			TransferServiceBridgeTopics.CreateServer,
-			sceneId,
+			config,
 		);
 	}
 
@@ -42,28 +43,34 @@ export class TransferService implements OnStart {
 	 * Transfers a player to the provided game. A server in the default scene will be selected, or a new one will be created.
 	 * @param player The player to transfer
 	 * @param gameId The gameId to transfer the player to
+	 * @param sceneId The sceneId to transfer the player to. Note that this is based on the scene the server was _started_ with,
+	 * not it's currently active scene. If no servers are available, a new server with this starting scene will be created.
 	 * @param serverTransferData JSON encodable object that will be provided to the server being joined
 	 * @param clientTransferData JSON encodable object that will be provided to the client on transfer
 	 */
 	public async TransferToGame(
 		player: Player,
 		gameId: string,
+		sceneId?: string,
 		serverTransferData?: unknown,
 		clientTransferData?: unknown,
 	): Promise<Result<undefined, undefined>> {
-		return await this.TransferGroupToGame([player], gameId, serverTransferData, clientTransferData);
+		return await this.TransferGroupToGame([player], gameId, sceneId, serverTransferData, clientTransferData);
 	}
 
 	/**
 	 * Transfers a group of players to the provided game. A server in the default scene will be selected, or a new one will be created.
 	 * @param player The players to transfer
 	 * @param gameId The gameId to transfer the players to
+	 * @param sceneId The sceneId to transfer the player to. Note that this is based on the scene the server was _started_ with,
+	 * not it's currently active scene. If no servers are available, a new server with this starting scene will be created.
 	 * @param serverTransferData JSON encodable object that will be provided to the server being joined
 	 * @param clientTransferData JSON encodable object that will be provided to the clients on transfer
 	 */
 	public async TransferGroupToGame(
 		players: readonly Player[],
 		gameId: string,
+		sceneId?: string,
 		serverTransferData?: unknown,
 		clientTransferData?: unknown,
 	): Promise<Result<undefined, undefined>> {
@@ -71,6 +78,7 @@ export class TransferService implements OnStart {
 			TransferServiceBridgeTopics.TransferGroupToGame,
 			players,
 			gameId,
+			sceneId,
 			serverTransferData,
 			clientTransferData,
 		);
