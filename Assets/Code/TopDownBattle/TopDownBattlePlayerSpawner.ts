2816007ac3dc4a719effad70a4e72cc0e53ba784
Tshadow05 <Tshadow05@gmail.com>
2024-06-17 15:23:39 -0700

    Fixed top down slime graphics

--- Assets/Code/TopDownBattle/TopDownBattlePlayerSpawner.ts
+++ Assets/Code/TopDownBattle/TopDownBattlePlayerSpawner.ts
@@ -7,7 +7,6 @@ import TopDownBattleCharacter from "./TopDownBattleCharacter";
 import TopDownBattleGame, { GameMode } from "./TopDownBattleGame";
 import { TopDownBattleEvents } from "./TopDownEvents";
 import { TopDownBattle } from "./TopDownEntryPoint";
-import { ItemStack } from "@Easy/Core/Shared/Inventory/ItemStack";
 
 export default class TopDownBattlePlayerSpawner extends AirshipBehaviour {
 	@Header("Templates")
@@ -98,7 +97,6 @@ export default class TopDownBattlePlayerSpawner extends AirshipBehaviour {
 						}
 					}
 				});
-				character.inventory.AddItem(new ItemStack("Weapon01"));
 			});
 
 			//Turn off the core loading screen
