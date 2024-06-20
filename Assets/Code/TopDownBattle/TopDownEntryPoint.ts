a0c6e1e54ca977abd007bdc63fcc2fc65ec23a78
Tshadow05 <Tshadow05@gmail.com>
2024-06-20 09:50:16 -0700

    Removing TopDownBattle from core

--- Assets/Code/TopDownBattle/TopDownEntryPoint.ts
+++
@@ -1,5 +0,0 @@
-import TopDownBattleGame from "./TopDownBattleGame";
-
-//There is only one game class so expose it for anyone to access
-export const TopDownBattle: TopDownBattleGame =
-  GameObject.Find("GameManager").GetAirshipComponent<TopDownBattleGame>()!;
--- Assets/Code/TopDownBattle/TopDownItems/TopDownRangedItem.ts
+++
@@ -1,3 +0,0 @@
-import { HeldItem } from "@Easy/Core/Shared/Item/HeldItems/HeldItem";
-
-export class TopDownBattleRangedItem extends HeldItem {}
