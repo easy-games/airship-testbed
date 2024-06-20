504f25f225e2e27b59ea12eb24c0e460a4351dad
Luke Chatton <chattonluke@gmail.com>
2024-06-19 18:22:45 -0700

    change colors

--- Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/Party/PartyCard.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/Party/PartyCard.ts
@@ -4,6 +4,8 @@ import { Dependency } from "@Easy/Core/Shared/Flamework";
 import { AirshipUrl } from "@Easy/Core/Shared/Util/AirshipUrl";
 import { Bin } from "@Easy/Core/Shared/Util/Bin";
 import { CanvasAPI, HoverState } from "@Easy/Core/Shared/Util/CanvasAPI";
+import { ColorUtil } from "@Easy/Core/Shared/Util/ColorUtil";
+import { Theme } from "@Easy/Core/Shared/Util/Theme";
 
 export default class PartyCard extends AirshipBehaviour {
 	public layoutElement!: LayoutElement;
@@ -17,15 +19,16 @@ export default class PartyCard extends AirshipBehaviour {
 
 	override Start(): void {
 		this.layoutElement.preferredHeight = 84;
+		this.layoutElement.gameObject.GetComponent<ImageWithRoundedCorners>()?.Refresh();
 		this.bin.AddEngineEventConnection(
 			CanvasAPI.OnHoverEvent(this.gameButton.gameObject, (hov) => {
 				this.gameArrow.transform
 					.TweenAnchoredPositionX(hov === HoverState.ENTER ? -10 : -20, 0.5)
 					.SetEaseBounceOut();
-				// this.gameArrow.color = hov === HoverState.ENTER ? Theme.primary : Theme.white;
-				// this.gameText.color = hov === HoverState.ENTER ? Theme.primary : Theme.white;
-				// this.gameButton.GetComponent<Image>()!.color =
-				// 	hov === HoverState.ENTER ? Theme.white : ColorUtil.HexToColor("191A1D");
+				this.gameArrow.color = hov === HoverState.ENTER ? Theme.primary : Theme.white;
+				this.gameText.color = hov === HoverState.ENTER ? Theme.primary : Theme.white;
+				this.gameButton.GetComponent<Image>()!.color =
+					hov === HoverState.ENTER ? Theme.white : ColorUtil.HexToColor("24242E");
 			}),
 		);
 		this.bin.AddEngineEventConnection(
@@ -38,14 +41,18 @@ export default class PartyCard extends AirshipBehaviour {
 	public SetLeaderStatus(userStatus: UserStatusData | undefined) {
 		if (userStatus === undefined) {
 			this.layoutElement.preferredHeight = 84;
+			this.layoutElement.gameObject.GetComponent<ImageWithRoundedCorners>()?.Refresh();
+			return;
+		}
+
+		if (userStatus.status !== UserStatus.IN_GAME) {
+			this.layoutElement.preferredHeight = 84;
+			this.layoutElement.gameObject.GetComponent<ImageWithRoundedCorners>()?.Refresh();
 			return;
 		}
 
 		this.layoutElement.preferredHeight = 124;
-		if (userStatus.status !== UserStatus.IN_GAME) {
-			this.layoutElement.preferredHeight = 84;
-			return;
-		}
+		this.layoutElement.gameObject.GetComponent<ImageWithRoundedCorners>()?.Refresh();
 
 		if (this.loadedGameImageId !== userStatus.game.icon) {
 			this.loadedGameImageId = userStatus.game.icon;
