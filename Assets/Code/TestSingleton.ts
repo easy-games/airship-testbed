41c8d01ba1794a571d417fcbeaa8997f056ac260
Luke Chatton <chattonluke@gmail.com>
2024-06-30 14:33:37 -0700

    pub

--- Assets/Code/TestSingleton.ts
+++
@@ -1,8 +0,0 @@
-import { CharactersSingleton } from "@Easy/Core/Shared/Character/CharactersSingleton";
-import { Flamework, Singleton } from "@Easy/Core/Shared/Flamework";
-
-@Singleton()
-export class TestSingleton {
-	private test = Flamework.id<TestSingleton>();
-	public constructor(private readonly singleton: CharactersSingleton) {}
-}
