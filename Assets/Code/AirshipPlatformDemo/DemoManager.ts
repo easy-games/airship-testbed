31923dbfd88349c1fe50b3c82dc2c2e4b4de9394
Luke Chatton <chattonluke@gmail.com>
2024-07-05 16:43:34 -0700

    inventory polish

--- Assets/Code/AirshipPlatformDemo/DemoManager.ts
+++ Assets/Code/AirshipPlatformDemo/DemoManager.ts
@@ -58,7 +58,7 @@ export default class DemoManager extends AirshipBehaviour {
 		if (Game.IsClient()) {
 			// Optional: use locked camera mode for first person support
 			Airship.CharacterCamera.SetCharacterCameraMode(CharacterCameraMode.Orbit);
-			Airship.CharacterCamera.SetFirstPerson(false);
+			// Airship.CharacterCamera.SetFirstPerson(false);
 			// Airship.inventory.SetUIEnabled(false);
 
 			// Display local player deaths
