6d0e4e7ab9d30e61f6f589e6678a47c3f8c9fe70
Tshadow05 <Tshadow05@gmail.com>
2024-07-08 16:16:57 -0700

    Added a trigger teleport script

---
+++ Assets/Code/AirshipPlatformDemo/MovementModifications/CharacterTeleporter.ts
@@ -0,0 +1,53 @@
+import { Airship } from "@Easy/Core/Shared/Airship";
+import Character from "@Easy/Core/Shared/Character/Character";
+import { Game } from "@Easy/Core/Shared/Game";
+
+export default class CharacterTeleporter extends AirshipBehaviour {
+    
+    public targetTeleporter!: CharacterTeleporter;
+    
+    private key = "Teleport";
+    private currentTargets: Map<number, Character> = new Map();
+    private recievedTargets: Map<number, Character> = new Map();
+
+    override Awake(): void {
+        Airship.Characters.onCharacterSpawned.Connect((character)=>{
+            if(character.IsLocalCharacter() || Game.IsServer()){
+                //Listen to custom data and modify movement based on values
+                character.OnBeginMove.Connect((customMoveData, inputData, tick, isReplay) => {
+                    if(customMoveData.get(this.key) && this.currentTargets.has(character.id)) {
+                        this.targetTeleporter.RecieveTarget(character);
+                        character.movement.Teleport(this.targetTeleporter.transform.position);
+                    }
+                });
+            }
+        });
+    }
+
+    public OnTriggerEnter(collider: Collider): void {
+        let character = collider.attachedRigidbody?.gameObject.GetAirshipComponent<Character>();
+        if(character && (Game.IsServer() || character?.IsLocalCharacter()) 
+            && !this.recievedTargets.has(character.id)){
+            this.currentTargets.set(character.id, character);
+        }
+    }
+
+    public OnTriggerStay(collider: Collider): void {
+        let character = collider.attachedRigidbody?.gameObject.GetAirshipComponent<Character>();
+        if(character && character?.IsLocalCharacter()){
+            character.AddCustomMoveData(this.key, true);
+        }
+    }
+
+    public OnTriggerExit(collider: Collider): void {
+        let character = collider.attachedRigidbody?.gameObject.GetAirshipComponent<Character>();
+        if(character && (Game.IsServer() || character?.IsLocalCharacter())){
+            this.currentTargets.delete(character.id);
+            this.recievedTargets.delete(character.id);
+        }
+    }
+
+    public RecieveTarget(character: Character){
+        this.recievedTargets.set(character.id, character);
+    }
+}
