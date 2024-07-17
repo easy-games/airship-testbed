d46e4567e8ff27d34cc761785e49869ed628bc4a
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-07-17 13:11:47 +1200

    Remove other stuff

--- Assets/AirshipPackages/@Easy/Core/Shared/Network/index.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/Network/index.ts
@@ -2,4 +2,3 @@ export { AirshipNetworkBehaviour } from "./AirshipNetworkBehaviour";
 export { TargetRpc } from "./TargetRpc";
 export { ServerRpc } from "./ServerRpc";
 export { ObserversRpc } from "./ObserversRpc";
-export { NetworkedField } from "./NetworkedField";
