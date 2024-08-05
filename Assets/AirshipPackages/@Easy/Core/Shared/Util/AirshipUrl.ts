352a6131a6976aa9657fecaa4c345419969ed5ea
Luke Chatton <chattonluke@gmail.com>
2024-08-05 11:37:57 -0700

    pull urls from C#

--- Assets/AirshipPackages/@Easy/Core/Shared/Util/AirshipUrl.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/Util/AirshipUrl.ts
@@ -1,8 +1,17 @@
+// Manual typings for the C# "AirshipPlatformURL" class.
+// This means we are getting all routes straight from C#.
+declare const AirshipPlatformUrl: {
+	GameCoordinator: string;
+	ContentService: string;
+	DataStoreService: string;
+	DeploymentService: string;
+	CDN: string;
+};
+
 export const AirshipUrl = {
-	GameCoordinatorSocket: "https://gc-edge-staging.easy.gg",
-	GameCoordinator: "https://game-coordinator-fxy2zritya-uc.a.run.app",
-	ContentService: "https://content-service-fxy2zritya-uc.a.run.app",
-	DataStoreService: "https://data-store-service-fxy2zritya-uc.a.run.app",
-	DeploymentService: "https://deployment-service-fxy2zritya-uc.a.run.app",
-	CDN: "https://cdn-staging.easy.gg",
+	GameCoordinator: AirshipPlatformUrl.GameCoordinator,
+	ContentService: AirshipPlatformUrl.ContentService,
+	DataStoreService: AirshipPlatformUrl.DataStoreService,
+	DeploymentService: AirshipPlatformUrl.DeploymentService,
+	CDN: AirshipPlatformUrl.CDN,
 };
