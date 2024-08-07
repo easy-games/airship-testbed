8941f2771e1c0d005a08ffcba4fb483ac213eef8
Luke Chatton <chattonluke@gmail.com>
2024-08-07 09:09:22 -0700

    pull api key from c#

--- Assets/AirshipPackages/@Easy/Core/Shared/Util/AirshipUrl.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/Util/AirshipUrl.ts
@@ -1,17 +1,19 @@
 // Manual typings for the C# "AirshipPlatformURL" class.
 // This means we are getting all routes straight from C#.
 declare const AirshipPlatformUrl: {
-	GameCoordinator: string;
-	ContentService: string;
-	DataStoreService: string;
-	DeploymentService: string;
-	CDN: string;
+	gameCoordinator: string;
+	contentService: string;
+	dataStoreService: string;
+	deploymentService: string;
+	cdn: string;
+	gameCdn: string;
 };
 
 export const AirshipUrl = {
-	GameCoordinator: AirshipPlatformUrl.GameCoordinator,
-	ContentService: AirshipPlatformUrl.ContentService,
-	DataStoreService: AirshipPlatformUrl.DataStoreService,
-	DeploymentService: AirshipPlatformUrl.DeploymentService,
-	CDN: AirshipPlatformUrl.CDN,
+	GameCoordinator: AirshipPlatformUrl.gameCoordinator,
+	ContentService: AirshipPlatformUrl.contentService,
+	DataStoreService: AirshipPlatformUrl.dataStoreService,
+	DeploymentService: AirshipPlatformUrl.deploymentService,
+	CDN: AirshipPlatformUrl.cdn,
+	GameCDN: AirshipPlatformUrl.gameCdn,
 };
