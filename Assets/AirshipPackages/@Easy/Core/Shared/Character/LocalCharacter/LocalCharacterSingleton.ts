d7ce40731df6df1fdc8d013b7eaea239cc965974
Luke Chatton <chattonluke@gmail.com>
2024-08-23 14:49:51 -0700

    disable flying for now

--- Assets/AirshipPackages/@Easy/Core/Shared/Character/LocalCharacter/LocalCharacterSingleton.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/Character/LocalCharacter/LocalCharacterSingleton.ts
@@ -77,8 +77,7 @@ export class LocalCharacterSingleton {
 					this.stateChanged.Fire(state);
 				}
 				if (this.sprintOverlayEmission) {
-					this.sprintOverlayEmission.enabled =
-						state === CharacterState.Sprinting;
+					this.sprintOverlayEmission.enabled = state === CharacterState.Sprinting;
 				}
 			});
 			bin.Add(() => {
@@ -110,7 +109,7 @@ export class LocalCharacterSingleton {
 					const dt = now - lastSpace;
 					if (dt < 0.3) {
 						lastSpace = 0;
-						this.characterMovement?.SetDebugFlying(!this.characterMovement.IsFlying());
+						// this.characterMovement?.SetDebugFlying(!this.characterMovement.IsFlying());
 					} else {
 						lastSpace = now;
 					}
