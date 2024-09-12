80426f8ed96240ba658d6f520a8a5d3b55541484
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-09-12 22:09:20 +1200

    Wrote the rest of the pass I need to get functional

--- Assets/AirshipPackages/@Easy/Core/Shared/Types/Compiler/AirshipScriptableRenderPass.d.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/Types/Compiler/AirshipScriptableRenderPass.d.ts
@@ -1,6 +1,52 @@
 /// <reference no-default-lib="true"/>
 
+type CastsToRTHandle = RTHandle | RenderTargetIdentifier | Texture | RenderTexture;
+
+type ScriptableRenderPassInput = unknown;
+type SortingCriteria = unknown;
+
+declare interface ScriptableRenderPass {
+	readonly clearColor: Color;
+	readonly clearFlag: RTClearFlags;
+	readonly colorAttachmentHandle: RTHandle;
+	readonly depthAttachmentHandle: RTHandle;
+	readonly colorAttachmentHandles: CSArray<RTHandle>;
+	readonly colorStoreActions: CSArray<RenderBufferStoreAction>;
+	readonly depthStoreAction: RenderBufferStoreAction;
+	readonly input: ScriptableRenderPassInput;
+	readonly profilingSampler: never;
+	readonly renderPassEvent: RenderPassEvent;
+
+	Blit(
+		cmd: CommandBuffer,
+		source: CastsToRTHandle,
+		target: CastsToRTHandle,
+		material: Material,
+		passIndex: int,
+	): void;
+	Blit(cmd: CommandBuffer, data: RenderingData, material: Material, passIndex?: int);
+	Blit(cmd: CommandBuffer, data: RenderingData, source: CastsToRTHandle, material: Material, passIndex?: int);
+
+	ConfigureClear(clearFlag: int, color: Color): void;
+	ConfigureInput(input: ScriptableRenderPassInput): void;
+	ConfigureColorStoreActions(actions: CSArray<RenderBufferStoreAction>): void;
+	ConfigureDepthStoreAction(action: RenderBufferStoreAction): void;
+	ResetTarget(): void;
+
+	CreateDrawingSettings(
+		shaderTagIdList: CSArray<ShaderTagId>,
+		renderingData: RenderingData,
+		sortingCriteria: unknown,
+	): void;
+	CreateDrawingSettings(
+		shaderTagId: ShaderTagId,
+		renderingData: RenderingData,
+		sortingCriteria: SortingCriteria,
+	): void;
+}
+
 declare abstract class AirshipScriptableRenderPass {
+	readonly renderPass: ScriptableRenderPass;
 	// readonly clearColor: Color;
 	// readonly clearFlag: RTClearFlags;
 	// readonly colorAttachmentHandle: RTHandle;
@@ -20,15 +66,4 @@ declare abstract class AirshipScriptableRenderPass {
 	protected OnCameraSetup?(cmd: CommandBuffer, renderingData: RenderingData): void;
 	protected OnCameraCleanup?(cmd: CommandBuffer): void;
 	protected Configure?(cmd: CommandBuffer, descriptor: RenderTextureDescriptor): void;
-
-	/**
-	 * @deprecated
-	 */
-	protected Blit?(
-		cmd: CommandBuffer,
-		source: RTHandle,
-		target: RTHandle,
-		material: Material,
-		passIndex: number,
-	): void;
 }
--- Assets/Rendering/TestRenderPass.ts
+++ Assets/Rendering/TestRenderPass.ts
@@ -1,57 +1,73 @@
 export default class ExampleBlurPass extends AirshipScriptableRenderPass {
 	@Header("Profiling")
+	// The profiler tag that will show up in the frame debugger.
 	public profilerTag = "Template Pass";
 
 	@Header("Pass Settings")
+	public renderPassEvent: RenderPassEvent;
+
+	/**
+	 * @summary The downsampling size of the blur
+	 */
 	@Range(1, 4)
 	@int
 	public downsample = 1;
 
+	/**
+	 * @summary The strength of this blur
+	 */
 	@Range(0, 20)
 	@int
 	public blurStrength = 5;
 
-	private colorBuffer: RenderTexture;
-	private temporaryBufferId = Shader.PropertyToID("_TemporaryBuffer");
+	private colorBuffer: RenderBuffer;
 
-	private material: Material;
+	// It is good to cache the shader property IDs here.
+	readonly blurStrengthId = Shader.PropertyToID("_BlurStrength");
+
+	@Header("Material")
+	public material: Material;
+
+	private temporaryBufferID = Shader.PropertyToID("_TemporaryBuffer");
+	private temporaryBuffer: RenderTargetIdentifier;
 
 	constructor() {
 		super();
-		print("Constructed");
-		this.blurStrength = 10;
+		this.material.SetInt(this.blurStrengthId, this.blurStrength);
 	}
 
-	// protected Configure(cmd: CommandBuffer, descriptor: RenderTextureDescriptor): void {
-	// 	print("Configure", cmd, descriptor.width, descriptor.width, descriptor.height);
-	// }
+	// Gets called by the renderer before executing the pass.
+	// Can be used to configure render targets and their clearing state.
+	// Can be user to create temporary render target textures.
+	// If this method is not overriden, the render pass will render to the active camera render target.
+	protected OnCameraSetup(cmd: CommandBuffer, renderingData: RenderingData): void {
+		// Grab the camera target descriptor. We will use this when creating a temporary render texture.
+		const descriptor = renderingData.cameraData.cameraTargetDescriptor;
+
+		// Downsample the original camera target descriptor.
+		// You would do this for performance reasons or less commonly, for aesthetics.
+		descriptor.width /= this.downsample;
+		descriptor.height /= this.downsample;
+
+		// Set the number of depth bits we need for our temporary render texture.
+		descriptor.depthBufferBits = 0;
+
+		// Grab the color buffer from the renderer camera color target.
+		this.colorBuffer = renderingData.cameraData.targetTexture.colorBuffer;
+
+		// Create a temporary render texture using the descriptor from above.
+		cmd.GetTemporaryRT(this.temporaryBufferID, descriptor, FilterMode.Bilinear);
+		this.temporaryBuffer = new RenderTargetIdentifier(this.temporaryBufferID);
+	}
+
 	protected Execute(cmd: CommandBuffer, renderingData: RenderingData): void {
-		print("cmd is", cmd.name, "data is", renderingData.lightData);
+		// Blit from the color buffer to a temporary buffer and back. This is needed for a two-pass shader.
+		this.renderPass.Blit(cmd, this.colorBuffer, this.temporaryBuffer, this.material, 0); // shader pass 0
+		this.renderPass.Blit(cmd, this.temporaryBuffer, this.colorBuffer, this.material, 1); // shader pass 1
 	}
 
-	// protected OnCameraSetup(cmd: CommandBuffer, renderingData: RenderingData): void {
-	// 	print(cmd, renderingData, renderingData.cameraData, renderingData.postProcessingData);
-	// }
-
-	// protected Execute(command: CommandBuffer): void {
-	// 	print("Execute", command);
-	// 	//print("Executing", command.name);
-	// }
-
-	// protected OnCameraSetup(cmd: CommandBuffer): void {
-	// 	print("Camera Setup ", cmd);
-	// 	// const descriptor = rd.cameraData.cameraTargetDescriptor;
-	// 	// descriptor.width /= this.downsample;
-	// 	// descriptor.height /= this.downsample;
-
-	// 	// descriptor.depthBufferBits = 0;
-
-	// 	// this.colorBuffer = rd.cameraData.targetTexture;
-	// }
-
-	// protected OnCameraCleanup(cmd: CommandBuffer): void {
-	// 	print("Camera cleanup", cmd);
-	// }
+	protected OnCameraCleanup(cmd: CommandBuffer): void {
+		// Since we created a temporary render texture in OnCameraSetup, we need to release the memory here to avoid a leak.
+		cmd.ReleaseTemporaryRT(this.temporaryBufferID);
+	}
 }
-
-class Test extends AirshipBehaviour {}
