46b01f78cd2895ffd3f9856c7557abf634eba2c0
liamlutton <lutton+gh@me.com>
2024-09-13 11:59:05 -0700

    Add DisableCoreActions

---
+++ Assets/AirshipPackages/@Easy/Core/Shared/Input/AirshipCoreAction.ts
@@ -0,0 +1,14 @@
+export enum CoreAction {
+    Forward = "Forward",
+    Left = "Left",
+    Back = "Back",
+    Right = "Right",
+    Jump = "Jump",
+    Sprint = "Sprint",
+    Crouch = "Crouch",
+    PrimaryAction = "PrimaryAction",
+    SecondaryAction = "SecondaryAction",
+    Inventory = "Inventory",
+    Interact = "Interact",
+    PushToTalk = "PushToTalk",
+}
