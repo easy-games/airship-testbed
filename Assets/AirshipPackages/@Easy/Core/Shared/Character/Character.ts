f3d80e0d96836381cda81b00e3db1244fb10f628
Rob-bie <deibbor@gmail.com>
2024-09-27 22:19:06 -0500

    Set fixed camera look direction on teleport.

--- Assets/AirshipPackages/@Easy/Core/Shared/Character/Character.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/Character/Character.ts
@@ -254,6 +254,9 @@ export default class Character extends AirshipBehaviour {
 
 		if (lookVector) {
 			this.movement.TeleportAndLook(pos, lookVector);
+			if (Game.IsClient()) {
+				Airship.Camera.humanoidCameraMode?.SetDirection(lookVector);
+			}
 		} else {
 			this.movement.Teleport(pos);
 		}
