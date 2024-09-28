621ac39aa215d91c75c3983cb0265959a3773923
Peter <95272937+MrChickenRocket@users.noreply.github.com>
2024-09-28 12:11:35 +1000

    Fixed other faces in triplanar

--- Assets/AirshipPackages/@Easy/CoreMaterials/Shaders/WorldPositionShadergraph.hlsl
+++ Assets/AirshipPackages/@Easy/CoreMaterials/Shaders/WorldPositionShadergraph.hlsl
@@ -2,12 +2,30 @@
 #define WORLDPOSITIONSHADERGRAPH
 
 
-void PositionToUV_float(float uvScale, float3 position, out float2 xPlane, out float2 yPlane, out float2 zPlane)
+void PositionToUV_float(float uvScale, float3 position, float3 normal, out float2 xPlane, out float2 yPlane, out float2 zPlane)
 {
 	position *= uvScale;
-	xPlane = float2(position.x, position.y);
-	yPlane = float2(position.z, position.y);
-	zPlane = float2(position.x, position.z);
+
+	if (normal.z > 0) {
+		xPlane = float2(-position.x, position.y);
+	} else {
+		xPlane = float2(position.x, position.y);
+	}
+	
+	if (normal.x > 0) {
+		yPlane = float2(position.z, position.y);
+	}
+	else {
+		yPlane = float2(-position.z, position.y);
+	}
+	
+	if (normal.y > 0) {
+		zPlane = float2(position.x, position.z);
+	}
+	else {
+		zPlane = float2(-position.x, position.z);
+	}
+	
 }
 
 
