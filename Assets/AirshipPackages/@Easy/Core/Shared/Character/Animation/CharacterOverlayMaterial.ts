82a75e16be56738a9f7ab0576cfc34f0fa18a611
Tshadow05 <Tshadow05@gmail.com>
2024-10-15 11:24:03 -0700

    Cleaned up overlay material script

--- Assets/AirshipPackages/@Easy/Core/Shared/Character/Animation/CharacterOverlayMaterial.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/Character/Animation/CharacterOverlayMaterial.ts
@@ -2,38 +2,43 @@ import { Bin } from "@Easy/Core/Shared/Util/Bin";
 
 export default class CharacterOverlayMaterial extends AirshipBehaviour {
 	@Header("Templates")
-	public defaultOverlayMaterialTemplate: Material; 
+	public defaultOverlayMaterialTemplate: Material;
 
 	@Header("References")
 	public accessoryBuilder: AccessoryBuilder;
 
 	private bin = new Bin();
-	private currentMeshes: Renderer[] = [];
+	private currentRenderers: Renderer[] = [];
 
 	override Start(): void {
-		print("Overlay start");
+		//print("Overlay start");
 		this.accessoryBuilder.SetCreateOverlayMeshOnCombine(true);
-		this.bin.Add(this.accessoryBuilder.OnMeshCombined.Connect((usedMeshCombiner, skinnedMesh, staticMesh)=>{
-			print("ON MESH COMBINE OVERLAY");
-			this.currentMeshes.clear();
-			if(usedMeshCombiner){
-				this.currentMeshes = [skinnedMesh, staticMesh];
-			}else{
-				//Individual accessories
-				let allMeshes = this.accessoryBuilder.GetAllAccessoryMeshes();
-				for(let i=0; i<allMeshes.Length; i++){
-					this.currentMeshes.push(allMeshes.GetValue(i));
+		this.bin.Add(
+			this.accessoryBuilder.OnMeshCombined.Connect((usedMeshCombiner, skinnedMesh, staticMesh) => {
+				//print("ON MESH COMBINE OVERLAY");
+				this.currentRenderers.clear();
+				if (usedMeshCombiner) {
+					this.currentRenderers = [skinnedMesh, staticMesh];
+				} else {
+					//Individual accessories
+					let allMeshes = this.accessoryBuilder.GetAllAccessoryMeshes();
+					for (let i = 0; i < allMeshes.Length; i++) {
+						this.currentRenderers.push(allMeshes.GetValue(i));
+					}
 				}
-			}
-			this.SetOverlayMaterial(this.defaultOverlayMaterialTemplate);
-		}));
-
+				this.SetOverlayMaterial(this.defaultOverlayMaterialTemplate);
+			}),
+		);
 	}
 
-	public SetOverlayMaterial(newMaterial: Material){
-		print("Setting overlay to mat: " + (newMaterial?.name??""));
-		for(let ren of this.currentMeshes){
+	public SetOverlayMaterial(newMaterial: Material) {
+		//print("Setting overlay to mat: " + (newMaterial?.name ?? ""));
+		for (let ren of this.currentRenderers) {
 			ren.SetMaterial(ren.materials.Length, newMaterial);
 		}
+	}
+
+	public GetAllRenderers() {
+		return this.currentRenderers;
 	}
 }
