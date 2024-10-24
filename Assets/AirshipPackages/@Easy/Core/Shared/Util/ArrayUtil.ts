3ac197547f83010990298499bcedf326cc8d2706
Jonathan Holmes <Vorlias@users.noreply.github.com>
2024-10-24 15:38:38 +1300

    Add `ArrayUtil.MapByKey`

--- Assets/AirshipPackages/@Easy/Core/Shared/Util/ArrayUtil.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/Util/ArrayUtil.ts
@@ -14,4 +14,21 @@ export class ArrayUtil {
 		});
 		return arrayA;
 	}
+
+	/**
+	 * Groups array values into a map by the specified key value
+	 * @param array The array to mapify
+	 * @param groupKey The key to use
+	 */
+	public static MapByKey<TValue extends object, TKey extends keyof TValue & string>(
+		array: ReadonlyArray<TValue>,
+		groupKey: TKey,
+	): ReadonlyMap<TValue[TKey], TValue> {
+		const map = new Map<TValue[TKey], TValue>();
+		for (const value of array) {
+			const applicator = value[groupKey];
+			map.set(applicator, value);
+		}
+		return map;
+	}
 }
