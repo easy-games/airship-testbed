a455da31ca715f004f9e4d05e3bd94adf4b04890
Jack Schaffner <jackwschaffner@gmail.com>
2024-10-29 14:34:12 -0700

    Add matchmaking group change signal

--- Assets/AirshipPackages/@Easy/Core/Client/Controllers/Airship/Matchmaking/MatchmakingController.ts
+++ Assets/AirshipPackages/@Easy/Core/Client/Controllers/Airship/Matchmaking/MatchmakingController.ts
@@ -1,18 +1,29 @@
-import { ClientBridgeApiGetGroupForSelf, ClientBridgeApiLeaveQueue, MatchmakingControllerBridgeTopics } from "@Easy/Core/Client/ProtectedControllers/Airship/Matchmaking/MatchmakingController";
+import {
+	ClientBridgeApiGetGroupForSelf,
+	ClientBridgeApiLeaveQueue,
+	MatchmakingControllerBridgeTopics,
+} from "@Easy/Core/Client/ProtectedControllers/Airship/Matchmaking/MatchmakingController";
 import { Platform } from "@Easy/Core/Shared/Airship";
 import { Group } from "@Easy/Core/Shared/Airship/Types/Outputs/AirshipMatchmaking";
 import { Controller } from "@Easy/Core/Shared/Flamework";
 import { Game } from "@Easy/Core/Shared/Game";
+import { Signal } from "@Easy/Core/Shared/Util/Signal";
 
 @Controller({})
 export class AirshipMatchmakingController {
+	public onGroupChange: Signal<Group> = new Signal();
+
 	constructor() {
 		if (!Game.IsClient()) return;
 
 		Platform.Client.Matchmaking = this;
+
+		contextbridge.callback(MatchmakingControllerBridgeTopics.OnGroupChange, (_, group) => {
+			this.onGroupChange.Fire(group);
+		});
 	}
 
-	protected OnStart(): void { }
+	protected OnStart(): void {}
 
 	/**
 	 * Gets the users current matchmaking group data if they are in a group, otherwise returns undefined.
--- Assets/AirshipPackages/@Easy/Core/Client/ProtectedControllers/Airship/Matchmaking/MatchmakingController.ts
+++ Assets/AirshipPackages/@Easy/Core/Client/ProtectedControllers/Airship/Matchmaking/MatchmakingController.ts
@@ -3,10 +3,12 @@ import { Controller } from "@Easy/Core/Shared/Flamework";
 import { Game } from "@Easy/Core/Shared/Game";
 import { AirshipUrl } from "@Easy/Core/Shared/Util/AirshipUrl";
 import { DecodeJSON, EncodeJSON } from "@Easy/Core/Shared/json";
+import { SocketController } from "../../Socket/SocketController";
 
 export const enum MatchmakingControllerBridgeTopics {
 	GetGroupForSelf = "MatchmakingController:GetGroupForSelf",
 	LeaveQueue = "MatchmakingController:LeaveQueue",
+	OnGroupChange = "MatchmakingController:OnGroupChange",
 }
 
 export type ClientBridgeApiGetGroupForSelf = () => Group | undefined;
@@ -14,26 +16,33 @@ export type ClientBridgeApiLeaveQueue = () => Group | undefined;
 
 @Controller({})
 export class ProtectedMatchmakingController {
-	constructor() {
+	constructor(private readonly socketController: SocketController) {
 		if (!Game.IsClient()) return;
 
-		contextbridge.callback<ClientBridgeApiGetGroupForSelf>(
-			MatchmakingControllerBridgeTopics.GetGroupForSelf,
-			(_) => this.GetCurrentGroup().expect()
+		contextbridge.callback<ClientBridgeApiGetGroupForSelf>(MatchmakingControllerBridgeTopics.GetGroupForSelf, (_) =>
+			this.GetCurrentGroup().expect(),
 		);
 
-		contextbridge.callback<ClientBridgeApiLeaveQueue>(
-			MatchmakingControllerBridgeTopics.LeaveQueue,
-			(_) => this.LeaveQueue().expect()
+		contextbridge.callback<ClientBridgeApiLeaveQueue>(MatchmakingControllerBridgeTopics.LeaveQueue, (_) =>
+			this.LeaveQueue().expect(),
 		);
+
+		this.socketController.On<Group>("game-coordinator/group-change", (data) => {
+			contextbridge.invoke(MatchmakingControllerBridgeTopics.OnGroupChange, LuauContext.Game, data);
+		});
 	}
 
 	public async GetCurrentGroup(): Promise<ReturnType<ClientBridgeApiGetGroupForSelf>> {
 		const currentGameId = Game.gameId;
-		const result = InternalHttpManager.GetAsync(`${AirshipUrl.GameCoordinator}/groups/game-id/${currentGameId}/self`);
+		const result = InternalHttpManager.GetAsync(
+			`${AirshipUrl.GameCoordinator}/groups/game-id/${currentGameId}/self`,
+		);
 
 		if (!result.success || result.statusCode > 299) {
-			warn(`An error occurred while trying to find group for game ${currentGameId}. Status Code: ${result.statusCode}.\n`, result.error);
+			warn(
+				`An error occurred while trying to find group for game ${currentGameId}. Status Code: ${result.statusCode}.\n`,
+				result.error,
+			);
 			throw result.error;
 		}
 
@@ -46,12 +55,18 @@ export class ProtectedMatchmakingController {
 
 	public async LeaveQueue(): Promise<ReturnType<ClientBridgeApiLeaveQueue>> {
 		const currentGameId = Game.gameId;
-		const result = InternalHttpManager.PostAsync(`${AirshipUrl.GameCoordinator}/matchmaking/queue/leave/self`, EncodeJSON({
-			gameId: currentGameId,
-		}));
+		const result = InternalHttpManager.PostAsync(
+			`${AirshipUrl.GameCoordinator}/matchmaking/queue/leave/self`,
+			EncodeJSON({
+				gameId: currentGameId,
+			}),
+		);
 
 		if (!result.success || result.statusCode > 299) {
-			warn(`An error occurred while trying to leave queue for game ${currentGameId}. Status Code: ${result.statusCode}.\n`, result.error);
+			warn(
+				`An error occurred while trying to leave queue for game ${currentGameId}. Status Code: ${result.statusCode}.\n`,
+				result.error,
+			);
 			throw result.error;
 		}
 
@@ -62,5 +77,5 @@ export class ProtectedMatchmakingController {
 		return DecodeJSON(result.data) as Group;
 	}
 
-	protected OnStart(): void { }
+	protected OnStart(): void {}
 }
