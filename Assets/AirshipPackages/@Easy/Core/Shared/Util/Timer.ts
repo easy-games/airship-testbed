fc3d4406d87cf405d1e027d67207d6e26bffd9ab
Stephen Leitnick <sleitnick@gmail.com>
2025-01-06 16:53:12 -0500

    Use task lib for interval and timeout functions

--- Assets/AirshipPackages/@Easy/Core/Shared/Util/Timer.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/Util/Timer.ts
@@ -5,27 +5,31 @@ export const OnLateUpdate = new Signal<[deltaTime: number]>().WithAllowYield(fal
 export const OnFixedUpdate = new Signal<[fixedDeltaTime: number]>().WithAllowYield(false);
 
 export function SetTimeout<T extends unknown[]>(duration: number, callback: (...args: T) => void, ...args: T) {
-	const triggerTime = Time.time + duration;
-	const disconnect = OnUpdate.Connect(() => {
-		if (Time.time >= triggerTime) {
-			disconnect();
-			callback(...args);
-		}
-	});
-	return disconnect;
+	const thread = task.delay(
+		duration,
+		(...args: T) => {
+			task.spawn(callback, ...args);
+		},
+		...args,
+	);
+
+	return () => {
+		task.cancel(thread);
+	};
 }
 
 export function SetInterval(interval: number, callback: Callback, immediate?: boolean) {
-	if (immediate) {
-		callback();
-	}
-	let nextTriggerTime = Time.time + interval;
-	const disconnect = OnUpdate.Connect(() => {
-		const now = Time.time;
-		if (now >= nextTriggerTime) {
-			nextTriggerTime = now + interval;
-			callback();
+	const thread = task.spawn(() => {
+		if (immediate) {
+			task.spawn(callback);
+		}
+		while (true) {
+			task.wait(interval);
+			task.spawn(callback);
 		}
 	});
-	return disconnect;
+
+	return () => {
+		task.cancel(thread);
+	};
 }
