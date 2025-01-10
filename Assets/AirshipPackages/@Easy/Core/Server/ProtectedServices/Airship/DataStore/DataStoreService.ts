35d302ca712b791b94ad5860817ef257cc706621
Jack Schaffner <jackwschaffner@gmail.com>
2025-01-09 18:10:18 -0800

    Add locking support

--- Assets/AirshipPackages/@Easy/Core/Server/ProtectedServices/Airship/DataStore/DataStoreService.ts
+++ Assets/AirshipPackages/@Easy/Core/Server/ProtectedServices/Airship/DataStore/DataStoreService.ts
@@ -1,3 +1,7 @@
+import {
+	AirshipDataStoreLockData,
+	AirshipDataStoreLockMode,
+} from "@Easy/Core/Shared/Airship/Types/Outputs/AirshipDataStore";
 import { Service } from "@Easy/Core/Shared/Flamework";
 import { Game } from "@Easy/Core/Shared/Game";
 import { AirshipUrl } from "@Easy/Core/Shared/Util/AirshipUrl";
@@ -6,11 +10,19 @@ export const enum DataStoreServiceBridgeTopics {
 	GetKey = "DataStore:GetKey",
 	SetKey = "DataStore:SetKey",
 	DeleteKey = "DataStore:DeleteKey",
+	SetLock = "DataStore:SetLock",
+	GetLockData = "DataStore:GetLockData",
 }
 
 export type ServerBridgeApiDataGetKey<T> = (key: string) => DataStoreRecord<T> | undefined;
 export type ServerBridgeApiDataSetKey<T> = (key: string, data: T, etag?: string) => DataStoreRecord<T>;
 export type ServerBridgeApiDataDeleteKey<T> = (key: string, etag?: string) => DataStoreRecord<T> | undefined;
+export type ServerBridgeApiDataSetLock = (
+	key: string,
+	mode?: AirshipDataStoreLockMode,
+	stealFromOwnerId?: string,
+) => boolean;
+export type ServerBridgeApiDataGetLockData = (key: string) => AirshipDataStoreLockData;
 
 export interface DataStoreRecord<T> {
 	value: T;
@@ -34,6 +46,17 @@ export class ProtectedDataStoreService {
 				return this.SetKey(key, data, etag).expect();
 			},
 		);
+
+		contextbridge.callback<ServerBridgeApiDataSetLock>(
+			DataStoreServiceBridgeTopics.SetLock,
+			(_, key, mode, stealId) => {
+				return this.SetLockForKey(key, mode, stealId).expect();
+			},
+		);
+
+		contextbridge.callback<ServerBridgeApiDataGetLockData>(DataStoreServiceBridgeTopics.GetLockData, (_, key) => {
+			return this.GetLockDataForKey(key).expect();
+		});
 
 		contextbridge.callback<ServerBridgeApiDataDeleteKey<unknown>>(
 			DataStoreServiceBridgeTopics.DeleteKey,
@@ -76,6 +99,36 @@ export class ProtectedDataStoreService {
 		}
 
 		return json.decode<{ record: DataStoreRecord<T> | undefined }>(result.data).record;
+	}
+
+	public async SetLockForKey(
+		key: string,
+		lockMode?: AirshipDataStoreLockMode,
+		stealFromOwnerId?: string,
+	): Promise<ReturnType<ServerBridgeApiDataSetLock>> {
+		const result = InternalHttpManager.PostAsync(
+			`${AirshipUrl.DataStoreService}/data/key/${key}/lock`,
+			json.encode({
+				lockMode,
+				forceIfWriterId: stealFromOwnerId,
+			}),
+		);
+		if (!result.success || result.statusCode > 299) {
+			warn(`Unable to lock data key. Status Code: ${result.statusCode}.\n`, result.error);
+			throw result.error;
+		}
+
+		return json.decode<{ success: boolean }>(result.data).success;
+	}
+
+	public async GetLockDataForKey(key: string): Promise<ReturnType<ServerBridgeApiDataGetLockData>> {
+		const result = InternalHttpManager.GetAsync(`${AirshipUrl.DataStoreService}/data/key/${key}/lock`);
+		if (!result.success || result.statusCode > 299) {
+			warn(`Unable to get lock data for key. Status Code: ${result.statusCode}.\n`, result.error);
+			throw result.error;
+		}
+
+		return json.decode<AirshipDataStoreLockData>(result.data);
 	}
 
 	protected OnStart(): void {}
--- Assets/AirshipPackages/@Easy/Core/Server/Services/Airship/DataStore/AirshipDataStoreService.ts
+++ Assets/AirshipPackages/@Easy/Core/Server/Services/Airship/DataStore/AirshipDataStoreService.ts
@@ -2,9 +2,15 @@ import {
 	DataStoreServiceBridgeTopics,
 	ServerBridgeApiDataDeleteKey,
 	ServerBridgeApiDataGetKey,
+	ServerBridgeApiDataGetLockData,
 	ServerBridgeApiDataSetKey,
+	ServerBridgeApiDataSetLock,
 } from "@Easy/Core/Server/ProtectedServices/Airship/DataStore/DataStoreService";
 import { Platform } from "@Easy/Core/Shared/Airship";
+import {
+	AirshipDataStoreLockData,
+	AirshipDataStoreLockMode,
+} from "@Easy/Core/Shared/Airship/Types/Outputs/AirshipDataStore";
 import { Service } from "@Easy/Core/Shared/Flamework";
 import { Game } from "@Easy/Core/Shared/Game";
 
@@ -93,7 +99,7 @@ export class AirshipDataStoreService {
 	 * @param callback The function that will be called to retrieve the new data value.
 	 * @returns The data that was associated with the provided key.
 	 */
-	public async GetAndSet<T extends object>(
+	public async GetAndSetKey<T extends object>(
 		key: string,
 		callback: (record?: T) => Promise<T | undefined> | T | undefined,
 	): Promise<T | undefined> {
@@ -162,7 +168,7 @@ export class AirshipDataStoreService {
 	 * true will delete the key.
 	 * @returns The data that was deleted. If no data was deleted, nothing will be returned.
 	 */
-	public async GetAndDelete<T extends object>(
+	public async GetAndDeleteKey<T extends object>(
 		key: string,
 		callback: (record: T) => Promise<boolean> | boolean,
 	): Promise<T | undefined> {
@@ -199,6 +205,149 @@ export class AirshipDataStoreService {
 			warn("Error retrieving updated value.", err);
 			throw "Error retrieving updated value.";
 		}
+	}
+
+	/**
+	 * Locks a data store key so that only the server that locked the key can write to it.
+	 * The lock mode can be specified to allow reads from other servers on locked keys.
+	 * Locks time out after 24 hours.
+	 *
+	 * This function performs additional checks and attempts to steal the lock if it can
+	 * be stolen safely.
+	 *
+	 * A lock can be stolen safely if the server that owns the lock is offline.
+	 *
+	 * @param key The key to lock.
+	 * @param mode The mode to lock the key with. Defaults to ReadWrite which disables both
+	 * reading and writing on all other servers.
+	 *
+	 * @returns True if the key was successfully locked to this server. False otherwise.
+	 */
+	public async LockKeyOrStealSafely(
+		key: string,
+		mode: AirshipDataStoreLockMode = AirshipDataStoreLockMode.ReadWrite,
+	): Promise<boolean> {
+		this.CheckKey(key);
+
+		if (Game.IsEditor()) {
+			warn("[Data Store] LockKey() is unavailable in editor.");
+			return true;
+		}
+
+		const gotLock = contextbridge.invoke<ServerBridgeApiDataSetLock>(
+			DataStoreServiceBridgeTopics.SetLock,
+			LuauContext.Protected,
+			key,
+			mode,
+		);
+
+		if (gotLock) return true;
+
+		const lockData = await this.GetLockDataForKey(key);
+		if (!lockData.locked) {
+			// Try once more to acquire lock since there's no lock data for the key.
+			return contextbridge.invoke<ServerBridgeApiDataSetLock>(
+				DataStoreServiceBridgeTopics.SetLock,
+				LuauContext.Protected,
+				key,
+				mode,
+			);
+		}
+
+		const server = await Platform.Server.ServerManager.GetServer(lockData.lockData.ownerId);
+		if (server) {
+			// Server that owns the lock is online. Assume that they are still using it.
+			return false;
+		}
+
+		// Attempt to steal the lock from the inactive server.
+		return contextbridge.invoke<ServerBridgeApiDataSetLock>(
+			DataStoreServiceBridgeTopics.SetLock,
+			LuauContext.Protected,
+			key,
+			mode,
+			lockData.lockData.ownerId,
+		);
+	}
+
+	/**
+	 * Locks a data store key so that only the server that locked the key can write to it.
+	 * The lock mode can be specified to allow reads from other servers on locked keys.
+	 * Locks time out after 24 hours.
+	 *
+	 * This function does not perform any additional actions in attempt to aquire the lock.
+	 *
+	 * @param key The key to lock.
+	 * @param mode The mode to lock the key with. Defaults to ReadWrite which disables both
+	 * reading and writing on all other servers.
+	 * @param stealFromOwnerId Steals the lock from this ownerId if the key is already locked.
+	 * @returns True if the key was successfully locked to this server. False otherwise.
+	 */
+	public async LockKey(
+		key: string,
+		mode: AirshipDataStoreLockMode = AirshipDataStoreLockMode.ReadWrite,
+		stealFromOwnerId?: string,
+	): Promise<boolean> {
+		this.CheckKey(key);
+
+		if (Game.IsEditor()) {
+			warn("[Data Store] LockKey() is unavailable in editor.");
+			return true;
+		}
+
+		return contextbridge.invoke<ServerBridgeApiDataSetLock>(
+			DataStoreServiceBridgeTopics.SetLock,
+			LuauContext.Protected,
+			key,
+			mode,
+			stealFromOwnerId,
+		);
+	}
+
+	/**
+	 * Unlocks a data store key so that all servers can read and write the key. If the
+	 * current game server does not own the lock, this operation will fail, unless you
+	 * steal the lock by providing the current ownerId of the lock.
+	 * @param key The key you wish to unlock.
+	 * @param stealFromOwnerId The ownerId of the current lock holder. Only provide this parameter if you want to steal the lock.
+	 * @returns True if the key was successfully unlocked. False otherwise.
+	 */
+	public async UnlockKey(key: string, stealFromOwnerId?: string): Promise<boolean> {
+		this.CheckKey(key);
+
+		if (Game.IsEditor()) {
+			warn("[Data Store] UnlockKey() is unavailable in editor.");
+			return true;
+		}
+
+		return contextbridge.invoke<ServerBridgeApiDataSetLock>(
+			DataStoreServiceBridgeTopics.SetLock,
+			LuauContext.Protected,
+			key,
+			undefined,
+			stealFromOwnerId,
+		);
+	}
+
+	/**
+	 * Returns information about the current lock holder for a given key. You can use this information to
+	 * decide if you wish to steal a lock.
+	 * @param key The key to get lock information for.
+	 * @returns The lock information.
+	 */
+	public async GetLockDataForKey(key: string): Promise<AirshipDataStoreLockData> {
+		this.CheckKey(key);
+
+		if (Game.IsEditor()) {
+			warn("[Data Store] GetLockDataForKey() is unavailable in editor.");
+			return { locked: false };
+		}
+
+		return contextbridge.invoke<ServerBridgeApiDataGetLockData>(
+			DataStoreServiceBridgeTopics.GetLockData,
+			LuauContext.Protected,
+			key,
+		);
 	}
 
 	/**
