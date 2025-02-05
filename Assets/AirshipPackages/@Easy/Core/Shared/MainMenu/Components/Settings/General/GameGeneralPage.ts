15539231b765ca9ae6f559b1d7f0a811c5e447e0
Luke Chatton <chattonluke@gmail.com>
2025-02-05 14:54:21 -0800

    new escape menu design

--- Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/Settings/General/GameGeneralPage.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/Settings/General/GameGeneralPage.ts
@@ -1,31 +1,82 @@
 import { Dependency } from "@Easy/Core/Shared/Flamework";
 import { Game } from "@Easy/Core/Shared/Game";
+import { Keyboard } from "@Easy/Core/Shared/UserInput";
+import { AirshipUrl } from "@Easy/Core/Shared/Util/AirshipUrl";
 import { Bin } from "@Easy/Core/Shared/Util/Bin";
 import { SettingsPageSingleton } from "../../../Singletons/SettingsPageSingleton";
 import MainMenuPageComponent from "../../MainMenuPageComponent";
 import { SettingsTab } from "../SettingsPageName";
 
 export default class GameGeneralPage extends MainMenuPageComponent {
-	public buttonsWrapper: RectTransform;
 	public settingsBtn: Button;
 	public keybindsBtn: Button;
+	public buttonsContainer: RectTransform;
+	public playerListBtn: Button;
+	public playerListBackBtn: Button;
+	public disconnectBtn: Button;
+
+	public generalPage: GameObject;
+	public playerListPage: GameObject;
+
+	@Header("Game")
+	public gameTitle: TMP_Text;
+	public gameDesc: TMP_Text;
+	public gameImg: RawImage;
 
 	private bin = new Bin();
+	private subPageBin = new Bin();
 
 	public OnEnable(): void {
-		this.buttonsWrapper.gameObject.SetActive(!Game.IsMobile());
-		if (!Game.IsMobile()) {
-			this.bin.Add(
-				this.settingsBtn.onClick.Connect(() => {
-					Dependency<SettingsPageSingleton>().Open(SettingsTab.Input);
-				}),
-			);
-			this.bin.Add(
-				this.keybindsBtn.onClick.Connect(() => {
-					Dependency<SettingsPageSingleton>().Open(SettingsTab.Keybinds);
-				}),
-			);
-		}
+		this.bin.Add(
+			this.settingsBtn.onClick.Connect(() => {
+				Dependency<SettingsPageSingleton>().Open(SettingsTab.Input);
+			}),
+		);
+		this.bin.Add(
+			this.keybindsBtn.onClick.Connect(() => {
+				Dependency<SettingsPageSingleton>().Open(SettingsTab.Keybinds);
+			}),
+		);
+		this.bin.Add(
+			this.playerListBtn.onClick.Connect(() => {
+				this.OpenPlayerListSubPage();
+			}),
+		);
+		this.bin.Add(
+			this.playerListBackBtn.onClick.Connect(() => {
+				this.CloseSubPage();
+			}),
+		);
+		this.bin.Add(
+			this.disconnectBtn.onClick.Connect(() => {
+				task.spawn(() => {
+					TransferManager.Instance.Disconnect();
+				});
+			}),
+		);
+		this.generalPage.SetActive(true);
+		this.playerListPage.SetActive(false);
+	}
+
+	public OpenPlayerListSubPage(): void {
+		this.generalPage.SetActive(false);
+		this.playerListPage.SetActive(true);
+		this.playerListPage.transform.localScale = Vector3.one.mul(1.1);
+		NativeTween.LocalScale(this.playerListPage.transform, Vector3.one, 0.12).SetEaseQuadOut();
+		this.subPageBin.Add(
+			Keyboard.OnKeyDown(Key.Escape, (e) => {
+				e.SetCancelled(true);
+				this.CloseSubPage();
+			}),
+		);
+	}
+
+	public CloseSubPage(): void {
+		this.subPageBin.Clean();
+		this.playerListPage.SetActive(false);
+		this.generalPage.SetActive(true);
+		this.generalPage.transform.localScale = Vector3.one.mul(1.1);
+		NativeTween.LocalScale(this.generalPage.transform, Vector3.one, 0.12).SetEaseQuadOut();
 	}
 
 	override GetTargetAnchoredPositionY(): number {
@@ -36,7 +87,21 @@ export default class GameGeneralPage extends MainMenuPageComponent {
 		}
 	}
 
-	override Start(): void {}
+	override Start(): void {
+		task.spawn(() => {
+			if (!Game.gameData) {
+				Game.onGameDataLoaded.Wait();
+			}
+			this.gameTitle.text = Game.gameData!.name;
+			this.gameDesc.text = Game.gameData!.description;
+
+			let thumbUrl = AirshipUrl.CDN + "/images/" + Game.gameData!.iconImageId + ".png";
+			print("url: " + thumbUrl);
+			const tex = Bridge.DownloadTexture2DYielding(thumbUrl);
+			this.gameImg.texture = tex;
+			NativeTween.GraphicAlpha(this.gameImg, 1, 0.12).SetEaseQuadOut();
+		});
+	}
 
 	override OnDestroy(): void {
 		this.bin.Clean();
