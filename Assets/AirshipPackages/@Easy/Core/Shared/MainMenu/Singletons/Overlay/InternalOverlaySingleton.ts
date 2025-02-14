170c21fb30c1b64d723e25653ac2dece59130ebf
Luke Chatton <chattonluke@gmail.com>
2025-02-14 11:17:27 -0800

    improve mic settings logic

--- Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Singletons/Overlay/InternalOverlaySingleton.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Singletons/Overlay/InternalOverlaySingleton.ts
@@ -1,13 +1,18 @@
 import { Asset } from "@Easy/Core/Shared/Asset";
 import { CoreRefs } from "@Easy/Core/Shared/CoreRefs";
 import { OnStart, Singleton } from "@Easy/Core/Shared/Flamework";
+import { Game } from "@Easy/Core/Shared/Game";
 
 @Singleton()
 export class InternalOverlaySingleton implements OnStart {
 	OnStart(): void {
-		const go = Object.Instantiate(
-			Asset.LoadAsset("Assets/AirshipPackages/@Easy/Core/Prefabs/UI/MobileControls/AirshipOverlayCanvas.prefab"),
-			CoreRefs.protectedTransform,
-		);
+		if (Game.IsInGame()) {
+			Object.Instantiate(
+				Asset.LoadAsset(
+					"Assets/AirshipPackages/@Easy/Core/Prefabs/UI/MobileControls/AirshipOverlayCanvas.prefab",
+				),
+				CoreRefs.protectedTransform,
+			);
+		}
 	}
 }
