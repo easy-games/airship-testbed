5703ceef9f3e6d8566edb896836207f049643492
Tshadow05 <Tshadow05@gmail.com>
2025-02-14 13:17:27 -0800

    Added manual renderer support to CharacterRenderer so we can use the render pass for more than just the AccessoryBuilder items. Also made it so viewing the character from above doesn't trigger alpha transparency

--- Assets/AirshipPackages/@Easy/Core/Shared/Character/CharacterRendering.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/Character/CharacterRendering.ts
@@ -10,6 +10,7 @@ export default class CharacterRendering extends AirshipBehaviour {
 	public behindWallsMat?: Material;
 
 	@Header("Variables")
+	public staticRenderers: Renderer[] = [];
 	@SerializeField()
 	private renderBehindWalls = false;
 	@SerializeField()
@@ -22,14 +23,14 @@ export default class CharacterRendering extends AirshipBehaviour {
 	private bin = new Bin();
 	private cameraBin = new Bin();
 	private character: Character;
-	private renderers: Renderer[] = [];
+	private accRenderers: Renderer[] = [];
 	private propertyBlock: MaterialPropertyBlock;
 	private lastSetAlpha = -1;
 
 	protected Awake(): void {
 		this.character = this.gameObject.GetAirshipComponent<Character>()!;
-		if (this.character?.accessoryBuilder) {
-			this.character.accessoryBuilder.SetCreateOverlayMeshOnCombine(true);
+		if (this.character) {
+			this.Init();
 		}
 	}
 
@@ -40,6 +41,26 @@ export default class CharacterRendering extends AirshipBehaviour {
 	protected OnDisable(): void {
 		this.bin.Clean();
 		this.cameraBin.Clean();
+	}
+
+	private Init() {
+		if (this.character.accessoryBuilder) {
+			this.character.accessoryBuilder.SetCreateOverlayMeshOnCombine(true);
+		}
+
+		this.character.WaitForInit();
+
+		this.propertyBlock = new MaterialPropertyBlock();
+		this.propertyBlock.SetFloat("_Transparency", 1);
+
+		//Setup manually specified renderers
+		const useWallRenders = this.IsUsingWallMat();
+		const useAlpha = this.IsUsingAlpha();
+		if (useWallRenders || useAlpha) {
+			for (let i = 0; i < this.staticRenderers.size(); i++) {
+				this.SetupRenderer(this.staticRenderers[i], useWallRenders, useAlpha);
+			}
+		}
 	}
 
 	public SetRenderMode(
@@ -59,6 +80,9 @@ export default class CharacterRendering extends AirshipBehaviour {
 			if (!this.character) {
 				//error("CharacterRendering component must be on the same game object as the Character component");
 				return;
+			} else {
+				//Setup character and wait for character init
+				this.Init();
 			}
 		}
 
@@ -79,17 +103,15 @@ export default class CharacterRendering extends AirshipBehaviour {
 				if (willCombine) {
 					return;
 				}
-				const wallRenders =
-					this.behindWallsMat !== undefined &&
-					(this.renderBehindWalls ||
-						(this.renderBehindWallsForLocalPlayer && this.character.IsLocalCharacter()));
+				const useWallRenders = this.IsUsingWallMat();
+				const useAlpha = this.IsUsingAlpha();
 				for (let i = 0; i < accessories.size(); i++) {
 					for (let j = 0; j < accessories[i].renderers.size(); j++) {
 						// print(
 						// 	"ACC ADDED j: " + j + ": " + accessories[i].renderers[j].transform.parent.gameObject.name,
 						// );
-						this.SetupRenderer(accessories[i].renderers[j], wallRenders);
-						this.renderers.push(accessories[i].renderers[j]);
+						this.SetupRenderer(accessories[i].renderers[j], useWallRenders, useAlpha);
+						this.accRenderers.push(accessories[i].renderers[j]);
 					}
 				}
 				this.lastSetAlpha = -1;
@@ -99,22 +121,23 @@ export default class CharacterRendering extends AirshipBehaviour {
 		//Mesh Combine Event so we can set materials
 		this.bin.Add(
 			this.character.accessoryBuilder.OnMeshCombined.Connect((usedCombine, skinnedMesh, staticMesh) => {
-				const wallRenders =
-					this.behindWallsMat !== undefined &&
-					(this.renderBehindWalls ||
-						(this.renderBehindWallsForLocalPlayer && this.character.IsLocalCharacter()));
-				const useAlpha =
-					this.transparencyMat ||
-					(this.character.IsLocalCharacter() && this.renderTransparentWhenCloseForLocalPlayer);
+				const useWallRenders = this.IsUsingWallMat();
+				const useAlpha = this.IsUsingAlpha();
 
-				if (wallRenders || useAlpha) {
+				if (useWallRenders || useAlpha) {
 					//Setup materials to render behind walls
 					this.lastSetAlpha = -1;
-					this.renderers = this.character.accessoryBuilder.GetAllAccessoryRenderers();
+					this.accRenderers = this.character.accessoryBuilder.GetAllAccessoryRenderers();
 					this.propertyBlock.SetFloat("_Transparency", 1);
 
-					for (let i = 0; i < this.renderers.size(); i++) {
-						this.SetupRenderer(this.renderers[i], wallRenders);
+					for (let i = 0; i < this.accRenderers.size(); i++) {
+						//TODO: Right now Mesh Renderers are not combined in the MeshCombine so
+						//if they have multiple submeshes this rendering technique will NOT work.
+						//Ignoring them for now
+						if (this.accRenderers[i] instanceof MeshRenderer) {
+							continue;
+						}
+						this.SetupRenderer(this.accRenderers[i], useWallRenders, useAlpha);
 					}
 				}
 			}),
@@ -126,52 +149,84 @@ export default class CharacterRendering extends AirshipBehaviour {
 				Airship.Camera.onCameraModeChanged.Connect((mode) => {
 					this.cameraBin.Clean();
 					this.cameraBin.Add(
-						mode.onTargetDistance.Connect((distance, flatDistance) => {
-							this.EvaluateCameraDistance(flatDistance);
+						mode.onTargetDistance.Connect((distance, camPos, targetPos) => {
+							this.EvaluateCameraDistance(distance, camPos, targetPos);
 						}),
 					);
 				}),
 			);
 			if (Airship.Camera.activeCameraMode) {
 				this.cameraBin.Add(
-					Airship.Camera.activeCameraMode?.onTargetDistance.Connect((distance, flatDistance) => {
-						this.EvaluateCameraDistance(flatDistance);
+					Airship.Camera.activeCameraMode?.onTargetDistance.Connect((distance, camPos, targetPos) => {
+						this.EvaluateCameraDistance(distance, camPos, targetPos);
 					}),
 				);
 			}
 		}
 	}
 
-	private SetupRenderer(ren: Renderer, wallRenders: boolean) {
+	public IsUsingWallMat() {
+		return (
+			this.behindWallsMat !== undefined &&
+			(this.renderBehindWalls || (this.renderBehindWallsForLocalPlayer && this.character.IsLocalCharacter()))
+		);
+	}
+
+	public IsUsingAlpha() {
+		return (
+			this.transparencyMat !== undefined &&
+			this.character.IsLocalCharacter() &&
+			this.renderTransparentWhenCloseForLocalPlayer
+		);
+	}
+
+	private SetupRenderer(ren: Renderer, useWallRenders: boolean, useAlpha: boolean) {
+		if (ren === undefined) {
+			return;
+		}
+
 		//Store depth in stencil
 		ren.SetMaterial(ren.materials.size(), this.stencilMat);
 
-		//Transparencyransparency
-		if (this.transparencyMat) {
+		//Transparency
+		if (useAlpha && this.transparencyMat) {
 			ren.SetMaterial(ren.materials.size(), this.transparencyMat);
 			ren.SetPropertyBlock(this.propertyBlock);
 		}
 
 		//Render behind walls
-		if (wallRenders && this.behindWallsMat) {
+		if (useWallRenders && this.behindWallsMat) {
 			ren.SetMaterial(ren.materials.size(), this.behindWallsMat);
 		}
 
 		ren.gameObject.layer = this.character.IsLocalCharacter() ? Layer.LOCAL_STENCIL_MASK : Layer.STENCIL_MASK;
 	}
 
-	protected EvaluateCameraDistance(distance: number) {
+	protected EvaluateCameraDistance(distance: number, camPos: Vector3, targetPos: Vector3) {
+		let flatDistance = Vector3.Distance(camPos.WithY(targetPos.y), targetPos);
+		let verticalMod = math.clamp01(camPos.y - targetPos.y);
+
 		if (this.transparencyMat && this.renderTransparentWhenCloseForLocalPlayer) {
-			let alpha = math.max(0, distance - this.transparentDistance) / this.transparentMargin;
+			let alpha = math.lerp(
+				math.max(0, flatDistance - this.transparentDistance) / this.transparentMargin,
+				1,
+				verticalMod,
+			);
 			if (this.lastSetAlpha !== alpha) {
 				this.lastSetAlpha = alpha;
 				this.propertyBlock.SetFloat("_Transparency", alpha);
-				for (let i = this.renderers.size() - 1; i >= 0; i--) {
-					if (this.renderers[i]) {
-						this.renderers[i].SetPropertyBlock(this.propertyBlock);
+				//Dynamically added renderers
+				for (let i = this.accRenderers.size() - 1; i >= 0; i--) {
+					if (this.accRenderers[i]) {
+						this.accRenderers[i].SetPropertyBlock(this.propertyBlock);
 					} else {
-						this.renderers.remove(i);
+						this.accRenderers.remove(i);
 					}
+				}
+
+				//Non changing renderers
+				for (let i = 0; i < this.staticRenderers.size(); i++) {
+					this.staticRenderers[i].SetPropertyBlock(this.propertyBlock);
 				}
 			}
 		} else {
