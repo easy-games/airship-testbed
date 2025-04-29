a68ec82ca21bbdca5cc8fdda3c8294878ee797cf
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2025-04-29 14:41:16 -0700

    Send old group data on onGroupChange

--- Assets/AirshipPackages/@Easy/Core/Client/Controllers/Airship/Matchmaking/MatchmakingController.ts
+++ Assets/AirshipPackages/@Easy/Core/Client/Controllers/Airship/Matchmaking/MatchmakingController.ts
@@ -7,11 +7,13 @@ import { Platform } from "@Easy/Core/Shared/Airship";
 import { Group } from "@Easy/Core/Shared/Airship/Types/Outputs/AirshipMatchmaking";
 import { Controller } from "@Easy/Core/Shared/Flamework";
 import { Game } from "@Easy/Core/Shared/Game";
+import inspect from "@Easy/Core/Shared/Util/Inspect";
 import { Signal } from "@Easy/Core/Shared/Util/Signal";
 
 @Controller({})
 export class AirshipMatchmakingController {
-	public readonly onGroupChange: Signal<Group> = new Signal();
+	public readonly onGroupChange: Signal<[newGroup: Group, oldGroup?: Group]> = new Signal();
+	private oldGroup: Group | undefined;
 
 	constructor() {
 		if (!Game.IsClient()) return;
@@ -19,7 +21,11 @@ export class AirshipMatchmakingController {
 		Platform.Client.Matchmaking = this;
 
 		contextbridge.callback(MatchmakingControllerBridgeTopics.OnGroupChange, (_, group) => {
-			this.onGroupChange.Fire(group);
+			const previous = this.oldGroup;
+			print("oldGroup: " + inspect(this.oldGroup));
+			print("newGroup: " + inspect(group));
+			this.onGroupChange.Fire(group, previous);
+			this.oldGroup = group;
 		});
 	}
 
