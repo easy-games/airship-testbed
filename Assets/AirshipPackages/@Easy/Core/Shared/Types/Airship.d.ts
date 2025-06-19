724e0c3a966fa603954cbbf8a9d4599fbb62b59b
Jack Schaffner <jackwschaffner@gmail.com>
2025-06-18 21:59:15 -0700

    Migrate to tick based simulation

--- Assets/AirshipPackages/@Easy/Core/Shared/Types/Airship.d.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/Types/Airship.d.ts
@@ -120,7 +120,7 @@ interface StateSnapshot {
 	/**
 	 * The time the snapshot was created. This time is local to the client/server that created it.
 	 */
-	time: number;
+	// time: number;
 	/**
 	 * The tick the snapshot was created. This tick is local to the client/server that created it.
 	 */
@@ -153,7 +153,7 @@ interface InputCommand {
 	/**
 	 * The time the input was created. This time is local to the client/server that created it.
 	 */
-	time: number;
+	// time: number;
 	/**
 	 * The tick the input was created. This tick is local to the client/server that created it.
 	 */
@@ -214,13 +214,13 @@ interface CharacterMovement extends Component {
 	// GetCurrentMoveInputData(): MoveInputData;
 	RequestResimulation(commandNumber: number): boolean;
 	/**
-	 * Get's the simulation time that generated the provided command number. This returns the time in the local
+	 * Get's the simulation tick that generated the provided command number. This returns the tick in the local
 	 * simulation timeline.
 	 *
 	 * **Note: This only works for command numbers which have been completed, meaning that using this function in the OnTick and passing the input command number
 	 * will result in 0 as the simulation time. The local simulation time for an input is always included with the command number in the input object.**
 	 * */
-	GetLocalSimulationTimeFromCommandNumber(commandNumber: number): number;
+	GetLocalSimulationTickFromCommandNumber(commandNumber: number): number;
 	/** If this character movement has final authority on character position and values. */
 	IsAuthority(): boolean;
 
@@ -251,10 +251,9 @@ interface CharacterMovement extends Component {
 
 interface AirshipSimulationManager extends MonoBehaviour {
 	replaying: boolean;
-	OnSetSnapshot(callback: (time: number) => void): EngineEventConnection;
-	OnTick(callback: (time: number, replay: boolean) => void): EngineEventConnection;
-	OnHistoryLifetimeReached(callback: (time: number) => void): EngineEventConnection;
-	GetLastSimulationTime(time: number): number;
+	OnSetSnapshot(callback: (tick: number) => void): EngineEventConnection;
+	OnTick(callback: (tick: number, replay: boolean) => void): EngineEventConnection;
+	OnHistoryLifetimeReached(callback: (tick: number) => void): EngineEventConnection;
 }
 
 interface AirshipSimulationManagerWithLagCompensation {
