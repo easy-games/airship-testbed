a07133a4049099b7f22fc1314f0f8f3dfed613e9
Jonathan Holmes <Vorlias@users.noreply.github.com>
2025-06-24 10:53:39 +1200

    More testing

--- Assets/Code/Components/Debugging/Inventory/InventoryClientSyncTest.ts
+++ Assets/Code/Components/Debugging/Inventory/InventoryClientSyncTest.ts
@@ -2,8 +2,15 @@ import { Airship } from "@Easy/Core/Shared/Airship";
 import { Game } from "@Easy/Core/Shared/Game";
 import Inventory from "@Easy/Core/Shared/Inventory/Inventory";
 
-const HUMAN_REFRESH_RATE = 0.15; // "Fastest possible reaction time" (which happens to also match our 'spawn cooldown' w/ the bug)
+export const enum InventoryTestType {
+	TransferToChestTooQuickly,
+	TransferBetweenSlotsTooQuickly,
+}
+
+const HUMAN_LATENCY = 0.15; // "Fastest possible reaction time" (which happens to also match our 'spawn cooldown' w/ the bug)
 export default class InventoryClientSyncTest extends AirshipBehaviour {
+	public testType = InventoryTestType.TransferToChestTooQuickly;
+
 	private transferThread?: thread;
 
 	override Start(): void {
@@ -14,31 +21,64 @@ export default class InventoryClientSyncTest extends AirshipBehaviour {
 			Airship.Inventory.onInventoryOpened.Connect((event) => {
 				if (event.inventory !== inventory) return;
 
-				this.transferThread = task.spawn(() => {
-					while (task.wait(HUMAN_REFRESH_RATE)) {
+				if (this.testType === InventoryTestType.TransferToChestTooQuickly) {
+					this.transferThread = task.spawn(() => {
+						while (true) {
+							const slot = Airship.Inventory.localInventory?.FindSlotWithItemType("Iron");
+							if (slot === undefined) {
+								return;
+							}
+
+							Airship.Inventory.MoveToInventory(Airship.Inventory.localInventory!, slot, inventory);
+							task.wait(HUMAN_LATENCY);
+						}
+					});
+				} else if (this.testType === InventoryTestType.TransferBetweenSlotsTooQuickly) {
+					this.transferThread = task.spawn(() => {
 						const slot = Airship.Inventory.localInventory?.FindSlotWithItemType("Iron");
-						if (!slot) return;
-						Airship.Inventory.MoveToInventory(Airship.Inventory.localInventory!, slot, inventory);
-					}
-				});
+						if (slot === undefined) return;
+
+						const targetSlot = Airship.Inventory.localInventory?.GetFirstOpenSlot();
+						if (targetSlot === undefined) return;
+
+						while (true) {
+							Airship.Inventory.MoveToSlot(
+								Airship.Inventory.localInventory!,
+								slot,
+								Airship.Inventory.localInventory!,
+								targetSlot,
+							);
+							task.wait(HUMAN_LATENCY);
+							Airship.Inventory.MoveToSlot(
+								Airship.Inventory.localInventory!,
+								targetSlot,
+								Airship.Inventory.localInventory!,
+								slot,
+							);
+							task.wait(HUMAN_LATENCY);
+						}
+					});
+				}
 			});
 			Airship.Inventory.onInventoryClosed.Connect((event) => {
 				if (event.inventory !== inventory) return;
-
-				if (this.transferThread) {
-					task.cancel(this.transferThread);
-					this.transferThread = undefined;
-				}
+				if (this.transferThread) this.transferThread = void task.cancel(this.transferThread);
 			});
 
-			const prompt = Airship.Input.CreateProximityPrompt("Interact", "Test Chest", "Test", this.transform);
+			let promptText: string;
+			switch (this.testType) {
+				case InventoryTestType.TransferToChestTooQuickly:
+					promptText = "Inventory Transfer Test";
+					break;
+				case InventoryTestType.TransferBetweenSlotsTooQuickly:
+					promptText = "Slot Transfer Test";
+					break;
+			}
+
+			const prompt = Airship.Input.CreateProximityPrompt("Interact", "Chest", promptText, this.transform);
 			prompt.onActivated.Connect(() => {
-				const closeInventory = Airship.Inventory.OpenExternalInventory(inventory);
+				Airship.Inventory.OpenExternalInventory(inventory);
 			});
 		}
 	}
-
-	protected Update(dt: number): void {}
-
-	override OnDestroy(): void {}
 }
