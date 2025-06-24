f846687780b81964e988528856e101bdc4131b3f
Luke Chatton <chattonluke@gmail.com>
2025-06-24 11:22:24 -0700

    float3

--- Assets/Code/Components/Debugging/PlayerRole.ts
+++ Assets/Code/Components/Debugging/PlayerRole.ts
@@ -1,32 +1,7 @@
 export default class PlayerRole extends AirshipBehaviour {
 	override Start(): void {
-		// Airship.Players.onPlayerJoined.Connect((player) => {
-		// 	if (player.orgRoleName) {
-		// 		print(`Player ${player.username} has org role ${player.orgRoleName}`);
-		// 	} else {
-		// 		print(`Player ${player.username} has no org role.`);
-		// 	}
-		// });
-		// Airship.Inventory.RegisterItem("WoodSword", {
-		// 	displayName: "Wood Sword",
-		// 	maxStackSize: 1,
-		// 	accessoryPaths: ["Assets/Resources/Prefabs/WoodSword.prefab"],
-		// 	// image: "Assets/Resources/ItemRenders/wood_sword.png",
-		// });
-		// if (Game.IsServer()) {
-		// 	Airship.Characters.ObserveCharacters((c) => {
-		// 		const inv = c.inventory;
-		// 		inv.AddItem(new ItemStack("WoodSword"));
-		// 		inv.AddItem(new ItemStack("WoodSword"));
-		// 		inv.AddItem(new ItemStack("WoodSword"));
-		// 		inv.AddItem(new ItemStack("WoodSword"));
-		// 	});
-		// }
-		// while (Airship.Camera.GetMode() === undefined) {
-		// 	task.wait();
-		// }
-		// const cam = Airship.Camera.GetMode<FixedCameraMode>()!;
-		// cam.SetZOffset(10);
+		const f = new float3(1, 1, 1);
+		print("float 3: " + f.x);
 	}
 
 	override OnDestroy(): void {}
