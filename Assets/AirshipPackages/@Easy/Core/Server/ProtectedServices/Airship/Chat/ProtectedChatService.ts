1954714c7b83821f2c4d569ba4635881653ec8a5
Luke Chatton <chattonluke@gmail.com>
2025-07-28 13:11:05 -0700

    max 500 chars & strip rich text for non devs

--- Assets/AirshipPackages/@Easy/Core/Server/ProtectedServices/Airship/Chat/ProtectedChatService.ts
+++ Assets/AirshipPackages/@Easy/Core/Server/ProtectedServices/Airship/Chat/ProtectedChatService.ts
@@ -3,13 +3,25 @@ import { OnStart, Service } from "@Easy/Core/Shared/Flamework";
 
 @Service({})
 export class ProtectedChatService implements OnStart {
-    OnStart(): void {
-        this.StartupServerChatListener();
-    }
+	OnStart(): void {
+		this.StartupServerChatListener();
+	}
 
-    private StartupServerChatListener() {
+	private StartupServerChatListener() {
 		CoreNetwork.ClientToServer.SendChatMessage.server.OnClientEvent((player, text) => {
-            contextbridge.broadcast<(msg: string, fromConnId: number) => void>("ProtectedChat:SendMessage", text, player.connectionId);
+			if (text.size() > 500) return;
+			if (player.orgRoleName === undefined) {
+				text = this.SanitizeText(text);
+			}
+			contextbridge.broadcast<(msg: string, fromConnId: number) => void>(
+				"ProtectedChat:SendMessage",
+				text,
+				player.connectionId,
+			);
 		});
+	}
+
+	private SanitizeText(msg: string): string {
+		return Bridge.RemoveRichText(msg);
 	}
 }
