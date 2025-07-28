b0e52cd45306cfc9d0e950827ec605a1622c725a
Luke Chatton <chattonluke@gmail.com>
2025-07-28 13:21:58 -0700

    add chat ratelimit

--- Assets/AirshipPackages/@Easy/Core/Server/ProtectedServices/Airship/Chat/ProtectedChatService.ts
+++ Assets/AirshipPackages/@Easy/Core/Server/ProtectedServices/Airship/Chat/ProtectedChatService.ts
@@ -1,10 +1,23 @@
 import { CoreNetwork } from "@Easy/Core/Shared/CoreNetwork";
 import { OnStart, Service } from "@Easy/Core/Shared/Flamework";
+import { ChatColor } from "@Easy/Core/Shared/Util/ChatColor";
+import { SetInterval } from "@Easy/Core/Shared/Util/Timer";
+
+const MESSAGES_PER_SECOND = 2;
 
 @Service({})
 export class ProtectedChatService implements OnStart {
+	private messageCounters = new Map<string, number>();
+
 	OnStart(): void {
 		this.StartupServerChatListener();
+
+		SetInterval(1, () => {
+			for (let [userId, count] of this.messageCounters) {
+				count = math.max(count - MESSAGES_PER_SECOND, 0);
+				this.messageCounters.set(userId, count);
+			}
+		});
 	}
 
 	private StartupServerChatListener() {
@@ -13,6 +26,17 @@ export class ProtectedChatService implements OnStart {
 			if (player.orgRoleName === undefined) {
 				text = this.SanitizeText(text);
 			}
+
+			// rate limit
+			let counter = this.messageCounters.get(player.userId) ?? 0;
+			if (counter >= MESSAGES_PER_SECOND) {
+				player.SendMessage(ChatColor.Red("You are sending messages too quickly."));
+				return;
+			}
+
+			counter++;
+			this.messageCounters.set(player.userId, counter);
+
 			contextbridge.broadcast<(msg: string, fromConnId: number) => void>(
 				"ProtectedChat:SendMessage",
 				text,
