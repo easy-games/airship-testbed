581c1c03a9681a00243b1186bbc498f527e5fa07
Luke Chatton <chattonluke@gmail.com>
2025-07-28 13:48:08 -0700

    Chat limits (#117)
    * max 500 chars & strip rich text for non devs
    
    * add chat ratelimit
    
    * strip new line
    
    * sanitize DM's and party chat
    
    * remove debug ...
    
    * 500 max characters on input field
    
    * better regex from vorlias

--- Assets/AirshipPackages/@Easy/Core/Server/ProtectedServices/Airship/Chat/ProtectedChatService.ts
+++ Assets/AirshipPackages/@Easy/Core/Server/ProtectedServices/Airship/Chat/ProtectedChatService.ts
@@ -1,15 +1,54 @@
 import { CoreNetwork } from "@Easy/Core/Shared/CoreNetwork";
 import { OnStart, Service } from "@Easy/Core/Shared/Flamework";
+import { ChatColor } from "@Easy/Core/Shared/Util/ChatColor";
+import { SetInterval } from "@Easy/Core/Shared/Util/Timer";
+
+const MESSAGES_PER_SECOND = 2;
 
 @Service({})
 export class ProtectedChatService implements OnStart {
-    OnStart(): void {
-        this.StartupServerChatListener();
-    }
+	private messageCounters = new Map<string, number>();
 
-    private StartupServerChatListener() {
-		CoreNetwork.ClientToServer.SendChatMessage.server.OnClientEvent((player, text) => {
-            contextbridge.broadcast<(msg: string, fromConnId: number) => void>("ProtectedChat:SendMessage", text, player.connectionId);
+	OnStart(): void {
+		this.StartupServerChatListener();
+
+		SetInterval(1, () => {
+			for (let [userId, count] of this.messageCounters) {
+				count = math.max(count - MESSAGES_PER_SECOND, 0);
+				this.messageCounters.set(userId, count);
+			}
 		});
+	}
+
+	private StartupServerChatListener() {
+		CoreNetwork.ClientToServer.SendChatMessage.server.OnClientEvent((player, text) => {
+			if (text.size() > 500) return;
+			if (player.orgRoleName === undefined || true) {
+				text = this.SanitizeText(text);
+			}
+
+			// rate limit
+			let counter = this.messageCounters.get(player.userId) ?? 0;
+			if (counter >= MESSAGES_PER_SECOND) {
+				player.SendMessage(ChatColor.Red("You are sending messages too quickly."));
+				return;
+			}
+
+			counter++;
+			this.messageCounters.set(player.userId, counter);
+
+			contextbridge.broadcast<(msg: string, fromConnId: number) => void>(
+				"ProtectedChat:SendMessage",
+				text,
+				player.connectionId,
+			);
+		});
+	}
+
+	public SanitizeText(msg: string): string {
+		msg = Bridge.RemoveRichText(msg);
+		msg = string.gsub(msg, "\\%d%d?%d?", "")[0]; // remove any \0 - \255 (ASCII)
+		msg = string.gsub(msg, "\\%a", "")[0]; // remove anything like \t, \a, \x etc.
+		return msg;
 	}
 }
