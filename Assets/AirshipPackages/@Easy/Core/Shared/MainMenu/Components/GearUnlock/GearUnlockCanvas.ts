4e073832440a7e9f3a3263ab2c9f1eddf8f895b0
Luke Chatton <chattonluke@gmail.com>
2025-12-19 09:29:59 -0800

    portrait mode is finished

---
+++ Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/GearUnlock/GearUnlockCanvas.ts
@@ -0,0 +1,28 @@
+export default class GearUnlockCanvas extends AirshipBehaviour {
+	public titleText: TMP_Text;
+	public messageText: TMP_Text;
+	public rawImage: RawImage;
+	public continueBtn: Button;
+	public wrapper: RectTransform;
+	public canvasGroup: CanvasGroup;
+
+	override Start(): void {}
+
+	public Init(gear: PlatformGear, title: string, message: string, renderTexture: RenderTexture): void {
+		this.titleText.text = title;
+		this.messageText.text = message;
+
+		this.rawImage.texture = renderTexture;
+		this.rawImage.enabled = true;
+
+		if (this.canvasGroup) {
+			this.canvasGroup.alpha = 0;
+			NativeTween.CanvasGroupAlpha(this.canvasGroup, 1, 0.3).SetEaseQuadOut();
+		}
+
+		this.wrapper.anchoredPosition = new Vector2(0, -60);
+		NativeTween.AnchoredPositionY(this.wrapper, 0, 0.4).SetEaseQuadOut();
+	}
+
+	override OnDestroy(): void {}
+}
