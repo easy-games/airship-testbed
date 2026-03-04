cc81609871b2c38bd41ec3bf1f58181f4b0e160b
Tshadow05 <Tshadow05@gmail.com>
2026-03-04 11:11:53 -0800

    Formatted new code

--- Assets/Code/VoxelWorldDemo/VoxelWorldPlantCharacter.ts
+++ Assets/Code/VoxelWorldDemo/VoxelWorldPlantCharacter.ts
@@ -5,64 +5,64 @@ import VoxelWorldPlantManager, { VoxelWorldPlantEvents } from "./VoxelWorldPlant
 import { Binding } from "@Easy/Core/Shared/Input/Binding";
 
 export default class VoxelWorldPlantCharacter extends AirshipBehaviour {
-    public character: Character;
+	public character: Character;
 
-    private plantManager: VoxelWorldPlantManager;
+	private plantManager: VoxelWorldPlantManager;
 
 	override Start(): void {
-        this.character.WaitForInit();
-        this.plantManager = VoxelWorldPlantManager.Get();
-        if(this.character.IsLocalCharacter()) { 
-            Airship.Input.CreateAction("AddVoxel", Binding.Key(Key.E));
-            Airship.Input.CreateAction("RemoveVoxel", Binding.Key(Key.Q));
-            Airship.Input.OnDown("AddVoxel").Connect(() => {
-                this.PlaceDirt();
-            })
+		this.character.WaitForInit();
+		this.plantManager = VoxelWorldPlantManager.Get();
+		if (this.character.IsLocalCharacter()) {
+			Airship.Input.CreateAction("AddVoxel", Binding.Key(Key.E));
+			Airship.Input.CreateAction("RemoveVoxel", Binding.Key(Key.Q));
+			Airship.Input.OnDown("AddVoxel").Connect(() => {
+				this.PlaceDirt();
+			});
 
-            Airship.Input.OnDown("RemoveVoxel").Connect(() => {
-                this.RemoveDirt();
-            })
+			Airship.Input.OnDown("RemoveVoxel").Connect(() => {
+				this.RemoveDirt();
+			});
 
-            Airship.Input.OnDown(CoreAction.Crouch).Connect(() => {
-                this.PlacePlant();
-            });
+			Airship.Input.OnDown(CoreAction.Crouch).Connect(() => {
+				this.PlacePlant();
+			});
 
-            Airship.Input.OnDown(CoreAction.Interact).Connect(() => {
-                this.ResetWorld();
-            });
-        }
+			Airship.Input.OnDown(CoreAction.Interact).Connect(() => {
+				this.ResetWorld();
+			});
+		}
 	}
 
-    private PlacePlant() { 
-        // If on a weed, damage weed
-        if(!this.plantManager.TryDamagePlant(this.character.transform.position)) {
-            // Otherwise try to place a plant
-            this.plantManager.TryPlacePlant(this.character.transform.position);
-        }
-    }
+	private PlacePlant() {
+		// If on a weed, damage weed
+		if (!this.plantManager.TryDamagePlant(this.character.transform.position)) {
+			// Otherwise try to place a plant
+			this.plantManager.TryPlacePlant(this.character.transform.position);
+		}
+	}
 
-    private PlaceDirt() {
-        print("Wants to place dirt")
-        if(this.plantManager.GetPlant(this.character.transform.position) !== undefined) { 
-            print("Plant is in the way")
-            return;
-        }
+	private PlaceDirt() {
+		print("Wants to place dirt");
+		if (this.plantManager.GetPlant(this.character.transform.position) !== undefined) {
+			print("Plant is in the way");
+			return;
+		}
 
-        VoxelWorldPlantEvents.RequestPlaceDirt.client.FireServer();
-    }
+		VoxelWorldPlantEvents.RequestPlaceDirt.client.FireServer();
+	}
 
-    private RemoveDirt() {
-        print("Wants to remove dirt")
-        if(this.plantManager.GetPlant(this.character.transform.position) !== undefined) { 
-            print("Plant is in the way")
-            return;
-        }
+	private RemoveDirt() {
+		print("Wants to remove dirt");
+		if (this.plantManager.GetPlant(this.character.transform.position) !== undefined) {
+			print("Plant is in the way");
+			return;
+		}
 
-        VoxelWorldPlantEvents.RequestDestroyDirt.client.FireServer();
-    }
+		VoxelWorldPlantEvents.RequestDestroyDirt.client.FireServer();
+	}
 
-    private ResetWorld() {
-        print("Client requests to reset world");
-        VoxelWorldPlantEvents.ResetWorld.client.FireServer();
-    }
+	private ResetWorld() {
+		print("Client requests to reset world");
+		VoxelWorldPlantEvents.ResetWorld.client.FireServer();
+	}
 }
