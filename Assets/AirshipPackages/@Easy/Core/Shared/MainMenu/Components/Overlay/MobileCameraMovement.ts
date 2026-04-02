47cce1a1100c2c01b0f868d3c548637868594c44
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2026-04-02 13:18:34 -0700

    Added tap register function that when called will not end a camera drag if another one is started.

--- Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/Overlay/MobileCameraMovement.ts
+++ Assets/AirshipPackages/@Easy/Core/Shared/MainMenu/Components/Overlay/MobileCameraMovement.ts
@@ -13,8 +13,9 @@ export default class MobileCameraMovement extends AirshipBehaviour {
 	private touchStartPos = Vector2.zero;
 	private touchStartRotX = 0;
 	private touchStartRotY = 0;
-	private touchPointerId = 0;
+	private touchPointerId = -1;
 	private image: Image;
+	private cameraDragFromRegisteredTouch = false;
 
 	protected Awake(): void {
 		this.image = this.gameObject.GetComponent<Image>()!;
@@ -25,11 +26,11 @@ export default class MobileCameraMovement extends AirshipBehaviour {
 
 		this.bin.AddEngineEventConnection(
 			CanvasAPI.OnBeginDragEvent(this.gameObject, (data) => {
+				this.cameraDragFromRegisteredTouch = false;
 				const camSystem = Dependency<AirshipCameraSingleton>().cameraSystem;
 				if (!camSystem) return;
 				const camMode = camSystem.GetMode();
 
-				// print("Begin drag. pointerId=" + data.pointerId + ", position=" + data.position);
 				this.touchPointerId = data.pointerId;
 				this.touchStartPos = data.position;
 				this.touchStartRotX = camMode.rotationX;
@@ -54,16 +55,100 @@ export default class MobileCameraMovement extends AirshipBehaviour {
 		this.image.enabled = false;
 	}
 
+	protected LateUpdate(dt: number): void {
+		if (!this.cameraDragFromRegisteredTouch || this.touchPointerId < 0) {
+			return;
+		}
+
+		for (let i = 0; i < Input.touchCount; i++) {
+			const t = Input.GetTouch(i);
+			if (t.fingerId !== this.touchPointerId) {
+				continue;
+			}
+			if (t.phase === TouchPhase.Ended || t.phase === TouchPhase.Canceled) {
+				this.touchPointerId = -1;
+				this.cameraDragFromRegisteredTouch = false;
+				return;
+			}
+			if (t.phase === TouchPhase.Moved || t.phase === TouchPhase.Stationary) {
+				const pos = new Vector2(t.position.x, t.position.y);
+				this.ApplyDragFromScreenPosition(pos);
+			}
+			return;
+		}
+
+		this.touchPointerId = -1;
+		this.cameraDragFromRegisteredTouch = false;
+	}
+
 	public SetActive(active: boolean) {
 		this.gameObject.SetActive(active);
 	}
 
+	/**
+	 * Use when we want a finger to continue to rotate the camera after a second camera drag happens.
+	 * This will be auto unregistered when the touch ends or is cancelled as well.
+	 */
+	public RegisterCameraDragFromTouchId(touchId: number): void {
+		if (this.touchPointerId === touchId) {
+			if (this.cameraDragFromRegisteredTouch) {
+				let found = false;
+				for (let i = 0; i < Input.touchCount; i++) {
+					const t = Input.GetTouch(i);
+					if (t.fingerId !== touchId) {
+						continue;
+					}
+					found = true;
+					if (t.phase === TouchPhase.Ended || t.phase === TouchPhase.Canceled) {
+						this.ClearRegisteredCameraDragForTouchId(touchId);
+					}
+					break;
+				}
+				if (!found) {
+					this.ClearRegisteredCameraDragForTouchId(touchId);
+				}
+			}
+			return;
+		}
+
+		for (let i = 0; i < Input.touchCount; i++) {
+			const t = Input.GetTouch(i);
+			if (t.fingerId !== touchId) {
+				continue;
+			}
+			if (t.phase === TouchPhase.Ended || t.phase === TouchPhase.Canceled) {
+				this.ClearRegisteredCameraDragForTouchId(touchId);
+				return;
+			}
+
+			const camSystem = Dependency<AirshipCameraSingleton>().cameraSystem;
+			if (!camSystem) {
+				return;
+			}
+			const camMode = camSystem.GetMode();
+			this.touchPointerId = touchId;
+			this.touchStartPos = new Vector2(t.position.x, t.position.y);
+			this.touchStartRotX = camMode.rotationX;
+			this.touchStartRotY = camMode.rotationY;
+			this.cameraDragFromRegisteredTouch = true;
+			return;
+		}
+	}
+
+	/** Immediately stops the registered camera drag for this touch id. */
+	public ClearRegisteredCameraDragForTouchId(touchId: number): void {
+		if (this.cameraDragFromRegisteredTouch && this.touchPointerId === touchId) {
+			this.touchPointerId = -1;
+			this.cameraDragFromRegisteredTouch = false;
+		}
+	}
+
 	public BeginDragEvent(data: PointerEventData) {
+		this.cameraDragFromRegisteredTouch = false;
 		const camSystem = Dependency<AirshipCameraSingleton>().cameraSystem;
 		if (!camSystem) return;
 		const camMode = camSystem.GetMode();
 
-		// print("Begin drag. pointerId=" + data.pointerId + ", position=" + data.position);
 		this.touchPointerId = data.pointerId;
 		this.touchStartPos = data.position;
 		this.touchStartRotX = camMode.rotationX;
@@ -71,14 +156,36 @@ export default class MobileCameraMovement extends AirshipBehaviour {
 	}
 
 	public DragEvent(data: PointerEventData) {
+		if (this.cameraDragFromRegisteredTouch) {
+			return;
+		}
+		if (this.touchPointerId !== data.pointerId) return;
+
+		this.ApplyDragFromScreenPosition(data.position);
+	}
+
+	public EndDragEvent(pointerId: number) {
+		if (this.touchPointerId === pointerId) {
+			this.touchPointerId = -1;
+			this.cameraDragFromRegisteredTouch = false;
+		}
+	}
+
+	public CancelDrag(): void {
+		this.touchPointerId = -1;
+		this.cameraDragFromRegisteredTouch = false;
+	}
+
+	public GetTouchPointerId(): number {
+		return this.touchPointerId;
+	}
+
+	private ApplyDragFromScreenPosition(screenPosition: Vector2): void {
 		const camSystem = Dependency<AirshipCameraSingleton>().cameraSystem;
 		if (!camSystem) return;
 		const camMode = camSystem.GetMode();
 
-		// print("Dragging. pointerId=" + data.pointerId + ", position=" + data.position);
-		if (this.touchPointerId !== data.pointerId) return;
-
-		const deltaPosSinceStart = data.position.sub(this.touchStartPos);
+		const deltaPosSinceStart = screenPosition.sub(this.touchStartPos);
 		const touchSensitivity = contextbridge.invoke<() => number>(
 			"ClientSettings:GetTouchSensitivity",
 			LuauContext.Protected,
@@ -89,19 +196,5 @@ export default class MobileCameraMovement extends AirshipBehaviour {
 			MIN_ROT_X,
 			MAX_ROT_X,
 		);
-	}
-
-	public EndDragEvent(pointerId: number) {
-		if (this.touchPointerId === pointerId) {
-			this.touchPointerId = -1;
-		}
-	}
-
-	public CancelDrag(): void {
-		this.touchPointerId = -1;
-	}
-
-	public GetTouchPointerId(): number {
-		return this.touchPointerId;
 	}
 }
